cf21ed072a4a8b83f69f435c2f5cfccdf4af3f3d
falexwolf <f.alex.wolf@gmx.de>
2017-11-07 13:50:19 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='anndata',
     version='0.1',
-    description='Single-Cell Analysis in Python.',
+    description='Class for storing an annotated data matrix.',
     url='http://github.com/theislab/anndata',
     author='F. Alexander Wolf, P. Angerer',
     author_email='alex.wolf@helmholtz-muenchen.de',
