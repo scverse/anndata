a1c23dd7402d4b238665d708b56aff6de3713aa3
falexwolf <f.alex.wolf@gmx.de>
2017-11-07 13:49:29 +0100

    register at pypi

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='anndata',
+    version='0.1',
+    description='Single-Cell Analysis in Python.',
+    url='http://github.com/theislab/anndata',
+    author='F. Alexander Wolf, P. Angerer',
+    author_email='alex.wolf@helmholtz-muenchen.de',
+    license='BSD-3-Clause',
+)
