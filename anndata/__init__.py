dd761a46bbbd65be955afaec4767b4f9215ecc40
falexwolf <f.alex.wolf@gmx.de>
2017-11-10 07:36:36 +0100

    added versioneer and requirements

--- anndata/__init__.py
+++ anndata/__init__.py
@@ -1,1 +1,5 @@
 from .ann_data import AnnData
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
--- setup.py
+++ setup.py
@@ -1,11 +1,39 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from pathlib import Path
+
+req_path = Path('requires.txt')
+if not req_path.is_file():
+    req_path = Path('scanpy.egg-info') / req_path
+with req_path.open() as requirements:
+    requires = [l.strip() for l in requirements]
 
 setup(
     name='anndata',
-    version='0.1',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(cmdclass),
     description='Class for storing an annotated data matrix.',
     url='http://github.com/theislab/anndata',
-    author='F. Alexander Wolf, P. Angerer',
+    author='Alex Wolf, Philipp Angerer',
     author_email='alex.wolf@helmholtz-muenchen.de',
     license='BSD-3-Clause',
+    install_requires=requires,
+    packages=find_packages(),
+    zip_safe=False,
+    classifiers=[
+        'Environment :: Console',
+        'Framework :: Jupyter',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD-3-Clause',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Data Analysis',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Visualization',
+    ],
 )
