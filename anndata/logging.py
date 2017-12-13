740155ce1338101b0b71016590ed76a57bd1225b
falexwolf <f.alex.wolf@gmx.de>
2017-12-13 01:55:51 +0100

    some simple logging

---
+++ anndata/logging.py
@@ -0,0 +1,31 @@
+import os
+
+_previous_memory_usage = -1
+
+
+def get_memory_usage():
+    import psutil
+    process = psutil.Process(os.getpid())
+    meminfo_attr = ('memory_info' if hasattr(process, 'memory_info')
+                    else 'get_memory_info')
+    mem = getattr(process, meminfo_attr)()[0] / 2**20  # output in MB
+    mem_diff = mem
+    global _previous_memory_usage
+    if _previous_memory_usage != -1:
+        mem_diff = mem - _previous_memory_usage
+    _previous_memory_usage = mem
+    return mem, mem_diff
+
+
+def format_memory_usage(mem_usage, msg='', newline=False):
+    mem, diff = mem_usage
+    string = (('\n' if newline else '')
+              + msg + (' \n... ' if msg != '' else '')
+              + 'Memory usage: current {:.2f} GB, difference {:+.2f} GB'
+              .format(mem, diff))
+    return string
+
+
+def print_memory_usage(msg='', newline=False):
+    string = format_memory_usage(get_memory_usage(), msg, newline)
+    print(string)
