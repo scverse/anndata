4cc78fc4336e9a0b5b1cb5d9812b62aed2e20205
falexwolf <f.alex.wolf@gmx.de>
2018-03-20 14:39:46 +0100

    added alex as main contributor also in setup

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     description='An annotated data matrix.',
     long_description=readme,
     url='http://github.com/theislab/anndata',
-    author='Philipp Angerer, Alex Wolf',
+    author='Alex Wolf, Philipp Angerer',
     author_email='alex.wolf@helmholtz-muenchen.de',
     license='BSD-3-Clause',
     install_requires=requires,
