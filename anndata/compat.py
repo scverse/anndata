e88da1dd1cd96f6235c0d3ba83859d2f13e8c2d9
Philipp A <flying-sheep@web.de>
2018-06-01 10:29:51 +0200

    Fixed 3.5 compat for path stuff

--- anndata/compat.py
+++ anndata/compat.py
@@ -1,28 +1,37 @@
+from abc import ABC
 from pathlib import Path
 from typing import Union
 
 try:
-	from os import PathLike, fspath
+    from os import PathLike, fspath
 except ImportError:
-	PathLike = Union[Path, str, bytes]
-	
-	def fspath(path: PathLike) -> Union[str, bytes]:
-		"""Copied from https://github.com/python/cpython/blob/3.6/Lib/os.py"""
-		if isinstance(path, (str, bytes)):
-			return path
-		
-		path_type = type(path)
-		try:
-			path_repr = path_type.__fspath__(path)
-		except AttributeError:
-			if hasattr(path_type, '__fspath__'):
-				raise
-			else:
-				raise TypeError("expected str, bytes or os.PathLike object, not " + path_type.__name__)
-		if isinstance(path_repr, (str, bytes)):
-			return path_repr
-		else:
-			raise TypeError(
-				"expected {}.__fspath__() to return str or bytes, not {}"
-				.format(path_type.__name__, type(path_repr).__name__)
-			)
+    class PathLike(ABC):
+        @classmethod
+        def __subclasshook__(cls, subclass):
+            return (
+                issubclass(subclass, (Path, str, bytes))
+                or hasattr(subclass, '__fspath__')
+            )
+
+    def fspath(path: PathLike) -> Union[str, bytes]:
+        """Copied from https://github.com/python/cpython/blob/3.6/Lib/os.py"""
+        if isinstance(path, (str, bytes)):
+            return path
+        if isinstance(path, Path):
+            return str(path)
+
+        path_type = type(path)
+        try:
+            path_repr = path_type.__fspath__(path)
+        except AttributeError:
+            if hasattr(path_type, '__fspath__'):
+                raise
+            else:
+                raise TypeError("expected str, bytes or os.PathLike object, not " + path_type.__name__)
+        if isinstance(path_repr, (str, bytes)):
+            return path_repr
+        else:
+            raise TypeError(
+                "expected {}.__fspath__() to return str or bytes, not {}"
+                .format(path_type.__name__, type(path_repr).__name__)
+            )
