b92e7c38d10f3df7e8ac263a3547f210f0dee189
falexwolf <f.alex.wolf@gmx.de>
2019-04-03 11:03:38 +0200

    maintain dtype during initialization

--- anndata/layers.py
+++ anndata/layers.py
@@ -13,7 +13,7 @@ class AnnDataLayers:
         self,
         adata: 'AnnData',
         layers: Optional[Mapping[str, np.ndarray]] = None,
-        dtype: Union[str, np.dtype] = 'float32',
+        dtype: Union[str, np.dtype] = None,
         adata_ref: 'AnnData' = None,
         oidx: 'Index' = None,
         vidx: 'Index' = None,
@@ -32,11 +32,10 @@ class AnnDataLayers:
                     .format(key, type(layer).__name__)
                 )
             if layer.shape != self._adata.shape:
-                raise ValueError('Shape does not fit.')
-            if layer.dtype != np.dtype(dtype):
+                raise ValueError('Shape of layer {} does not fit.'.format(key))
+            if dtype is not None:
                 self._layers[key] = layer.astype(dtype, copy=False)
-            else:
-                self._layers[key] = layer
+            self._layers[key] = layer
 
     def __getitem__(self, key):
         if self.isview:
