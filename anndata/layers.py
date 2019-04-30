e55e6cda2a1f82eead2a2fec5c8f2ab187d46113
Isaac Virshup <ivirshup@gmail.com>
2019-04-30 22:35:25 +1000

    draft, squash

--- anndata/layers.py
+++ anndata/layers.py
@@ -1,11 +1,181 @@
-from typing import Mapping, Optional, Union
+from abc import ABC, abstractmethod
+from collections import OrderedDict
+from collections.abc import MutableMapping
+from typing import Mapping, Optional, Union, Tuple
 
 import numpy as np
-from collections import OrderedDict
 from scipy.sparse import issparse
 
 if False:
     from .base import AnnData, Index  # noqa
+
+
+class AlignedMapping(MutableMapping, ABC):
+
+    @property
+    @abstractmethod
+    def isview(self):
+        pass
+
+    @abstractmethod # This could probably just get implemented here
+    def _validate_value(self, val, key) -> "NoneType":
+        """Raises an error if value is invalid"""
+        pass
+
+    # TODO
+    # I need to figure out what these idx value are
+    # def _validate_value(self, val, key):
+    #     for curidx, vallen in zip(self.idx, value.shape):
+    #         if curlen == slice(None):
+    #             continue
+    #         curl
+
+
+    @abstractmethod
+    def view(self, parent, idx):
+        """Return a view of the object"""
+        pass
+
+    @property
+    @abstractmethod
+    def parent(self) -> "AnnData":
+        """The AnnData this is connected to"""
+        pass
+
+    @property
+    @abstractmethod
+    def idx(self) -> Tuple:
+        """How to slice it"""
+        pass
+
+    @property
+    @abstractmethod
+    def attrname(self) -> str:
+        """What attr for the AnnData is this?"""
+        pass
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}: with keys: {', '.join(self._data.keys())}"
+
+    def as_dict(self):
+        return dict(self.items())
+
+class AlignedViewMixin:
+    @property
+    def isview(self):
+        return True
+
+    @property
+    def parent_mapping(self):
+        return getattr(self.parent._adata_ref, self.attrname)
+
+    def __getitem__(self, key):
+        return self.parent_mapping[key][self.idx]
+
+    def __delitem__(self, key):
+        adata = self.parent.copy()
+        getattr(adata, self.attrname).__delitem__(key)
+        self.parent._init_as_actual(adata)
+
+    def __setitem__(self, key, value):
+        adata = self.parent.copy()
+        getattr(adata, self.attrname).__setitem__(key, value)
+        self.parent._init_as_actual(adata)
+
+    def view(self, parent: "AnnData", idx):
+        """Returns a subsetted view of this object"""
+        return self.__class__(self, parent, idx)
+
+class AlignedActualMixin:
+    @property
+    def isview(self):
+        return False
+
+    def __getitem__(self, key):
+        return self._data[key]
+
+    def __setitem__(self, key, value):
+        self._validate_value(value, key)
+        self._data[key] = value
+
+    def __delitem__(self, key):
+        del self._data[key]
+
+class LayersBase(AlignedMapping):
+    @property
+    def attrname(self):
+        return "layers"
+
+class Layers(LayersBase, AlignedActualMixin):
+    pass
+
+class LayersView(LayersBase, AlignedViewMixin):
+    pass
+
+class AxisArraysBase(AlignedMapping):
+    _dimnames = ("obs", "var")
+
+    @property
+    def dim(self):
+        return self._dimnames[self._axis]
+
+    @property
+    def attrname(self):
+        return "{}m".format(self.dim)
+
+class AxisArrays(AxisArraysBase, AlignedActualMixin):
+    pass
+
+class AxisArraysView(AxisArraysBase, AlignedViewMixin):
+    pass
+
+# class LayersBase(AlignedMapping):
+#     # def __init__(
+#         # self,
+#         # adata
+#     # )
+#     # def view(self, subset):
+
+
+#     def __iter__(self):
+#         return self._data.__iter__()
+
+#     def __len__(self):
+#         return self._data.__len__()
+
+
+# class LayersView(LayersBase):
+#     def __init__(self, parent, oidx, vidx):
+#         self.parent = parent
+#         self.parent_layer = parent_layer
+#         self._oidx = oidx
+#         self._vidx = vidx
+
+#     def __getitem__(self, key):
+#         return self._adata_ref.layers[key][self._oidx, self._vidx]
+
+# class Layers(LayersBase):
+#     def __init__(
+#         self,
+#         parent: "AnnData",
+#         layers: Mapping = {}
+#     ):
+#         self.parent = parent
+#         self.obs_names = parent.var_names
+#         self.var_names = parent.obs_names
+#         self._data = {}
+#         self.update(layers)
+
+#     def __getitem__(self, key):
+#         return self._data[key]
+
+#     def __setitem__(self, key, value):
+#         if value.shape != self.parent.shape:
+#             raise ValueError()
+#         self._data[key] = value
+
+#     def __delitem__(self, key):
+#         del self._data[key]
 
 
 class AnnDataLayers:
