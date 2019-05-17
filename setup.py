25c50e41e6061fa0e2631dff4963bff2f4316aa8
Isaac Virshup <ivirshup@gmail.com>
2019-05-17 15:14:38 +1000

    Bump minimum python version to 3.6

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
         'Topic :: Scientific/Engineering :: Bio-Informatics',
         'Topic :: Scientific/Engineering :: Visualization',
     ],
