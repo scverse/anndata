e9ccfc33fa4152ad4e137dea4e5700386c0e4da0
Philipp A <flying-sheep@web.de>
2019-06-08 12:38:28 +0200

    Remove os

--- anndata/tests/test_h5py.py
+++ anndata/tests/test_h5py.py
@@ -1,22 +1,20 @@
 # Had to rename the test module to h5py_ so that it wouldn't conflict with the
 # h5py import upon testing.
 
-import os
-from tempfile import mkstemp
-
 import numpy as np
 import scipy.sparse as ss
 
 from anndata import h5py
 
 
-def test_create_and_read_dataset():
-    h5_path = mkstemp(suffix=".h5")[1]
-    sparse_matrix = ss.csr_matrix([[0, 1, 0],
-                                   [0, 0, 1],
-                                   [0, 0, 0],
-                                   [1, 1, 0]],
-                                  dtype=np.float64)
+def test_create_and_read_dataset(tmp_path):
+    h5_path = tmp_path / 'test.h5'
+    sparse_matrix = ss.csr_matrix([
+        [0, 1, 0],
+        [0, 0, 1],
+        [0, 0, 0],
+        [1, 1, 0],
+    ], dtype=np.float64)
     with h5py.File(h5_path) as h5f:
         h5f.create_dataset('sparse/matrix', data=sparse_matrix)
     with h5py.File(h5_path) as h5f:
@@ -27,17 +25,16 @@ def test_create_and_read_dataset():
         assert (h5f['sparse']['matrix'][:-2] != sparse_matrix[:-2]).size == 0
         assert (h5f['sparse']['matrix'].value != sparse_matrix).size == 0
 
-    os.remove(h5_path)
 
-
-def test_create_dataset_from_dataset():
-    from_h5_path = mkstemp(suffix=".h5")[1]
-    to_h5_path = mkstemp(suffix=".h5")[1]
-    sparse_matrix = ss.csr_matrix([[0, 1, 0],
-                                   [0, 0, 1],
-                                   [0, 0, 0],
-                                   [1, 1, 0]],
-                                  dtype=np.float64)
+def test_create_dataset_from_dataset(tmp_path):
+    from_h5_path = tmp_path / 'from.h5'
+    to_h5_path = tmp_path / 'to.h5'
+    sparse_matrix = ss.csr_matrix([
+        [0, 1, 0],
+        [0, 0, 1],
+        [0, 0, 0],
+        [1, 1, 0],
+    ], dtype=np.float64)
     with h5py.File(from_h5_path) as from_h5f:
         from_dset = from_h5f.create_dataset('sparse/matrix', data=sparse_matrix)
 
@@ -45,25 +42,22 @@ def test_create_dataset_from_dataset():
             to_h5f.create_dataset('sparse/matrix', data=from_dset)
             assert (to_h5f['sparse/matrix'].value != sparse_matrix).size == 0
 
-    os.remove(from_h5_path)
-    os.remove(to_h5_path)
 
-
-def test_dataset_append():
-    h5_path = mkstemp(suffix=".h5")[1]
-    sparse_matrix = ss.csr_matrix([[0, 1, 0],
-                                   [0, 0, 1],
-                                   [0, 0, 0],
-                                   [1, 1, 0]],
-                                  dtype=np.float64)
-    to_append = ss.csr_matrix([[0, 1, 1],
-                               [1, 0, 0]],
-                              dtype=np.float64)
+def test_dataset_append(tmp_path):
+    h5_path = tmp_path / 'test.h5'
+    sparse_matrix = ss.csr_matrix([
+        [0, 1, 0],
+        [0, 0, 1],
+        [0, 0, 0],
+        [1, 1, 0],
+    ], dtype=np.float64)
+    to_append = ss.csr_matrix([
+        [0, 1, 1],
+        [1, 0, 0],
+    ], dtype=np.float64)
     appended_matrix = ss.vstack((sparse_matrix, to_append))
 
     with h5py.File(h5_path) as h5f:
         h5f.create_dataset('matrix', data=sparse_matrix, chunks=(100000,))
         h5f['matrix'].append(to_append)
         assert (h5f['matrix'].value != appended_matrix).size == 0
-
-    os.remove(h5_path)
