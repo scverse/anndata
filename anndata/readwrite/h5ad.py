10356f4a6e1d0f45761c15449bb3b1eb79f8e4ca
Isaac Virshup <ivirshup@gmail.com>
2019-09-24 15:53:58 +1000

    Fix inplace on disk sparse to dense transition
    This would be triggered by something like:
    
    ```python
    adata = ad.read_h5ad("sparse_X.h5ad", backed=True)
    adata.write(force_dense=True)
    ```
    
    Now, the dense dataset is written to a temporary dataset, which latter replaces the original via hardlink.

--- anndata/readwrite/h5ad.py
+++ anndata/readwrite/h5ad.py
@@ -89,6 +89,11 @@ def write_h5ad(
     if adata.isbacked:  # close so that we can reopen below
         adata.file.close()
     with h5py.File(filepath, mode) as f:
+        if sparse_X_as_dense and isinstance(adata.X, SparseDataset) and Path(adata.filename) == Path(filepath):
+            write_sparse_as_dense(f, "_X", adata.X, dataset_kwargs)
+            del f["X"]
+            f["X"] = f["_X"]
+            del f["_X"]
         if sparse_X_as_dense and isinstance(adata.X, (sparse.spmatrix, SparseDataset)):
             if "X" in f: del f["X"]  # Normally handled by write_attribute
             write_sparse_as_dense(f, "X", adata.X, dataset_kwargs)
@@ -170,7 +175,6 @@ def write_sparse_compressed(f, key, value, fmt: str, dataset_kwargs=MappingProxy
     g = f.create_group(key)
     g.attrs["encoding-type"] = f"{fmt}_matrix"
     g.attrs["encoding-version"] = "0.1.0"
-    g.attrs["dtype"] = str(value.dtype)
     g.attrs["shape"] = value.shape
 
     g.create_dataset("data", data=value.data, **dataset_kwargs)
@@ -356,7 +360,7 @@ def read_h5ad(
         if X_dset is None:
             pass
         elif isinstance(X_dset, h5py.Group):
-            d["dtype"] = X_dset.attrs["dtype"]
+            d["dtype"] = X_dset["data"].dtype
         elif hasattr(X_dset, "dtype"):
             d["dtype"] = f["X"].dtype
         else:
