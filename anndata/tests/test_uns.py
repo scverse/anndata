b333b3316cd4f5e5792b2cedaabf5f0c4774ec51
Isaac Virshup <ivirshup@gmail.com>
2019-11-26 12:57:36 +1100

    Add more cases for color resetting

--- anndata/tests/test_uns.py
+++ anndata/tests/test_uns.py
@@ -6,17 +6,24 @@ from anndata import AnnData
 
 def test_uns_color_subset():
     # Tests for https://github.com/theislab/anndata/issues/257
-    # Tests when the length of the color vector doesn't match with the number of categories
     obs = pd.DataFrame(index=[f"cell{i}" for i in range(5)])
-    obs["cat"] = pd.Series(list("aabcd"), index=obs.index, dtype="category")
-    uns = {"cat_colors": ["red", "green", "blue"]}
+    obs["cat1"] = pd.Series(list("aabcd"), index=obs.index, dtype="category")
+    obs["cat2"] = pd.Series(list("aabbb"), index=obs.index, dtype="category")
+    uns = {
+        "cat1_colors": ["red", "green", "blue"],
+        "cat2_colors": ["red", "green", "blue"],
+    }
 
     adata = AnnData(np.ones((5, 5)), obs=obs, uns=uns)
 
+    # If number of categories does not match number of colors, they should be
+    # reset
     v = adata[:, [0, 1]]
-    assert "cat_colors" not in v.uns
+    assert "cat1_colors" not in v.uns
+    assert "cat2_colors" not in v.uns
 
-    adata.uns["cat_colors"] = ["red", "green", "blue", "yellow"]
+    # Otherwise the colors should still match after reseting
+    adata.uns["cat1_colors"] = ["red", "green", "blue", "yellow"]
     v = adata[[0, 1], :]
-    assert len(v.uns["cat_colors"]) == 1
-    assert v.uns["cat_colors"][0] == "red"
+    assert len(v.uns["cat1_colors"]) == 1
+    assert v.uns["cat1_colors"][0] == "red"
