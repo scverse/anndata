3f65152eca0f655fb91da5d1b310ca59dcc23df7
Philipp A <flying-sheep@web.de>
2020-01-24 11:36:07 +0100

    "

--- anndata/tests/test_utils.py
+++ anndata/tests/test_utils.py
@@ -12,8 +12,8 @@ from anndata._io.utils import report_read_key_on_error, AnnDataReadError
 @pytest.mark.parametrize(
     "group_fn",
     [
-        pytest.param(lambda _: zarr.group(), id='zarr'),
-        pytest.param(lambda p: h5py.File(p / "test.h5", mode="a"), id='h5py'),
+        pytest.param(lambda _: zarr.group(), id="zarr"),
+        pytest.param(lambda p: h5py.File(p / "test.h5", mode="a"), id="h5py"),
     ],
 )
 def test_key_error(tmp_path, group_fn):
@@ -22,7 +22,7 @@ def test_key_error(tmp_path, group_fn):
         raise NotImplementedError()
 
     group = group_fn(tmp_path)
-    with group if hasattr(group, '__enter__') else suppress():
+    with group if hasattr(group, "__enter__") else suppress():
         group["X"] = [1, 2, 3]
         group.create_group("group")
         with pytest.raises(AnnDataReadError) as e:
@@ -35,14 +35,14 @@ def test_key_error(tmp_path, group_fn):
 
 def test_clean_uns():
     d = dict(
-        uns=dict(species_categories=['a', 'b']),
+        uns=dict(species_categories=["a", "b"]),
         obs=dict(species=pd.Series([0, 1, 0])),
         var=dict(species=pd.Series([0, 1, 0, 2])),
     )
     _clean_uns(d)
     assert "species_categories" not in d["uns"]
     assert isinstance(d["obs"]["species"], pd.Categorical)
-    assert d["obs"]["species"].tolist() == ['a', 'b', 'a']
+    assert d["obs"]["species"].tolist() == ["a", "b", "a"]
     # var’s categories were overwritten by obs’s,
     # which we can detect here because var has too high codes
     assert isinstance(d["var"]["species"], pd.Series)
