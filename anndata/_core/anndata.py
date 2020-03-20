a80db25c738302b3c0d2f81b873d03c71e2682d3
Isaac Virshup <ivirshup@gmail.com>
2020-03-20 20:47:00 +1100

    Revert changes to __contains__ for DeprecatedDict

--- anndata/_core/anndata.py
+++ anndata/_core/anndata.py
@@ -1451,7 +1451,7 @@ class AnnData(metaclass=utils.DeprecationMixinMeta):
                 var=self.var.copy(),
                 # deepcopy on DictView does not work and is unnecessary
                 # as uns was copied already before
-                uns=self.uns.copy()
+                uns=self._uns.copy()
                 if isinstance(self.uns, DictView)
                 else deepcopy(self.uns),
                 obsm=self.obsm.copy(),
