e588f41ecd9a36c5943589835ba122ae40ffd83f
Koncopd <koncopd@gmail.com>
2020-11-24 19:14:32 +0100

    proper shapes and reprs

--- anndata/_core/concat_obs.py
+++ anndata/_core/concat_obs.py
@@ -43,14 +43,18 @@ class _ConcatViewMixin:
 
 
 class MapObsView:
-    def __init__(self, attr, adatas, adatas_oidx, reverse, vidx=None):
+    def __init__(self, attr, adatas, keys, adatas_oidx, reverse, vidx=None):
         self.adatas = adatas
+        self.keys = keys
         self.adatas_oidx = adatas_oidx
         self.reverse = reverse
         self.vidx = vidx
         self.attr = attr
 
     def __getitem__(self, key):
+        if key not in self.keys:
+            raise KeyError(f'No {key} in {self.attr} view')
+
         arrs = []
         for i, oidx in enumerate(self.adatas_oidx):
             if oidx is not None:
@@ -62,9 +66,13 @@ class MapObsView:
 
         return _arr[self.reverse] if self.reverse is not None else _arr
 
+    def __repr__(self):
+        descr = f"View of {self.attr} with keys: {str(self.keys)[1:-1]}"
+        return descr
+
 
 class AnnDataConcatView(_ConcatViewMixin):
-    def __init__(self, adatas, limits, resolved_idx, obs_names, var_names):
+    def __init__(self, adatas, limits, resolved_idx, obs_names, var_names, attrs_keys):
         self.adatas = adatas
         self.limits = limits
         self.obs_names = obs_names
@@ -72,11 +80,17 @@ class AnnDataConcatView(_ConcatViewMixin):
 
         self.adatas_oidx, self.oidx, self.vidx, self.reverse = resolved_idx
 
-        self.layers_view = MapObsView(
-            "layers", adatas, self.adatas_oidx, self.reverse, self.vidx
-        )
-        self.obsm_view = MapObsView("obsm", adatas, self.adatas_oidx, self.reverse)
-        self.obs_view = MapObsView("obs", adatas, self.adatas_oidx, self.reverse)
+        for attr, keys in attrs_keys.items():
+            set_vidx = self.vidx if attr == "layers" else None
+            setattr(
+                self,
+                attr + "_view",
+                MapObsView(
+                    attr, adatas, keys, self.adatas_oidx, self.reverse, set_vidx
+                )
+            )
+
+        self._attrs_keys = attrs_keys
 
     @property
     def X(self):
@@ -103,7 +117,7 @@ class AnnDataConcatView(_ConcatViewMixin):
 
     @property
     def shape(self):
-        return len(self.oidx), self.adatas[0].n_vars
+        return len(self.oidx), len(self.var_names)
 
     def __getitem__(self, index: Index):
         oidx, vidx = _normalize_indices(index, self.obs_names, self.var_names)
@@ -115,7 +129,16 @@ class AnnDataConcatView(_ConcatViewMixin):
             resolved_idx,
             self.obs_names[oidx],
             self.var_names[vidx],
+            self._attrs_keys,
         )
+
+    def __repr__(self):
+        n_obs, n_vars = self.shape
+        descr = f"AnnDataConcatView object with n_obs × n_vars = {n_obs} × {n_vars}"
+        for attr, keys in self._attrs_keys.items():
+            if len(keys) > 0:
+                descr += f"\n    {attr}: {str(keys)[1:-1]}"
+        return descr
 
 
 class AnnDataConcatObs(_ConcatViewMixin):
@@ -149,9 +172,25 @@ class AnnDataConcatObs(_ConcatViewMixin):
         if not self.obs_names.is_unique:
             logger.info("Observation names are not unique.")
 
+        self._attrs_keys = {}
+        for attr in ["obs", "obsm", "layers"]:
+            self._attrs_keys[attr] = list(getattr(adatas[0], attr).keys())
+
         for a in adatas[1:]:
             if not adatas[0].var_names.equals(a.var_names):
                 raise ValueError("Variables in the adatas are different.")
+            for attr, keys in self._attrs_keys.items():
+                ai_attr = getattr(a, attr)
+                a0_attr = getattr(adatas[0], attr)
+                new_keys = []
+                for key in keys:
+                    if key in ai_attr.keys():
+                        a0_ashape = a0_attr[key].shape
+                        ai_ashape = ai_attr[key].shape
+                        if len(a0_ashape) < 2 or a0_ashape[1] == ai_ashape[1]:
+                            new_keys.append(key)
+                self._attrs_keys[attr] = new_keys
+
         self.var_names = adatas[0].var_names
 
         self.adatas = adatas
@@ -172,8 +211,22 @@ class AnnDataConcatObs(_ConcatViewMixin):
             resolved_idx,
             self.obs_names[oidx],
             self.var_names[vidx],
+            self._attrs_keys,
         )
 
     @property
     def shape(self):
         return self.limits[-1], self.adatas[0].n_vars
+
+    def __repr__(self):
+        n_obs, n_vars = self.shape
+        descr = f"AnnDataConcatObs object with n_obs × n_vars = {n_obs} × {n_vars}"
+        for attr, keys in self._attrs_keys.items():
+            if len(keys) > 0:
+                descr += f"\n    view of {attr}: {str(keys)[1:-1]}"
+
+        obs_keys = list(self.obs.keys())
+        if len(obs_keys) > 0:
+            descr += f"\n    obs: {str(obs_keys)[1:-1]}"
+
+        return descr
