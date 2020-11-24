39ebfb119ad4993718e27fc797064782bae05e35
Koncopd <koncopd@gmail.com>
2020-11-24 17:12:27 +0100

    proper subsetting

--- anndata/_core/concat_obs.py
+++ anndata/_core/concat_obs.py
@@ -3,52 +3,80 @@ import numpy as np
 import pandas as pd
 
 from .index import _normalize_indices, Index
+from .views import _resolve_idx
 from .merge import concat_arrays
 from ..logging import anndata_logger as logger
 
 
-def merge(arrs):
+def _merge(arrs):
     rxers = [lambda x, fill_value, axis: x] * len(arrs)
     return concat_arrays(arrs, rxers)
 
 
+class _IndxViewMixin:
+    def _resolve_idx(self, oidx, vidx):
+        reverse = None
+        adatas_oidx = []
+
+        old_oidx = getattr(self, "oidx", None)
+        if old_oidx is not None:
+            oidx = _resolve_idx(old_oidx, oidx, self.limits[-1])
+
+        if isinstance(oidx, slice):
+            start, stop, step = oidx.indices(self.limits[-1])
+            u_oidx = np.arange(start, stop, step)
+        else:
+            if oidx.size > 1 and not np.all(np.diff(oidx) > 0):
+                u_oidx, reverse = np.unique(oidx, return_inverse=True)
+            else:
+                u_oidx = oidx
+
+        for lower, upper in zip([0] + self.limits, self.limits):
+            mask = (u_oidx >= lower) & (u_oidx < upper)
+            adatas_oidx.append(u_oidx[mask] - lower if mask.any() else None)
+
+        old_vidx = getattr(self, "vidx", None)
+        if old_vidx is not None:
+            vidx = _resolve_idx(old_vidx, vidx, self.adatas[0].n_vars)
+
+        return adatas_oidx, oidx, vidx, reverse
+
+
 class MapObsView:
-    def __init__(self, attr, adatas, adatas_oidx, reverse, vidx):
+    def __init__(self, attr, adatas, adatas_oidx, reverse, vidx=None):
         self.adatas = adatas
         self.adatas_oidx = adatas_oidx
         self.reverse = reverse
         self.vidx = vidx
         self.attr = attr
 
-    def set_index(adatas_oidx, reverse, vidx):
-        self.adatas_oidx = adatas_oidx
-        self.reverse = reverse
-        self.vidx = vidx
-
     def __getitem__(self, key):
-        print(self.vidx)
         arrs = []
         for i, oidx in enumerate(self.adatas_oidx):
             if oidx is not None:
                 arr = getattr(self.adatas[i], self.attr)[key]
-                idx = (oidx, self.vidx) if len(arr.shape) > 1 else oidx
+                idx = (oidx, self.vidx) if self.vidx is not None else oidx
                 arrs.append(arr[idx])
 
-        _arr = merge(arrs)
+        _arr = _merge(arrs)
 
         return _arr[self.reverse] if self.reverse is not None else _arr
 
 
-class AnnDataConcatView:
-    def __init__(self, adatas, adatas_oidx, reverse, vidx):
+class AnnDataConcatView(_IndxViewMixin):
+    def __init__(self, adatas, limits, resolved_idx, obs_names, var_names):
         self.adatas = adatas
-        self.adatas_oidx = adatas_oidx
-        self.reverse = reverse
-        self.vidx = vidx
+        self.limits = limits
+        self.obs_names = obs_names
+        self.var_names = var_names
 
-        self.obsm_view = MapObsView("obsm", adatas, adatas_oidx, reverse, vidx)
-        self.obs_view = MapObsView("obs", adatas, adatas_oidx, reverse, vidx)
-        self.layers_view = MapObsView("layers", adatas, adatas_oidx, reverse, vidx)
+        self.adatas_oidx, self.oidx, self.vidx, self.reverse = resolved_idx
+
+        self.layers_view = MapObsView(
+            "layers", adatas, self.adatas_oidx, self.reverse, self.vidx
+        )
+        self.obsm_view = MapObsView("obsm", adatas, self.adatas_oidx, self.reverse)
+        self.obs_view = MapObsView("obs", adatas, self.adatas_oidx, self.reverse)
 
     @property
     def X(self):
@@ -57,7 +85,7 @@ class AnnDataConcatView:
             if oidx is not None:
                 Xs.append(self.adatas[i].X[oidx, self.vidx])
 
-        _X = merge(Xs)
+        _X = _merge(Xs)
 
         return _X[self.reverse] if self.reverse is not None else _X
 
@@ -73,17 +101,24 @@ class AnnDataConcatView:
     def obs(self):
         return self.obs_view
 
-    def set_index(self, adatas_oidx, reverse, vidx):
-        self.adatas_oidx = adatas_oidx
-        self.reverse = reverse
-        self.vidx = vidx
+    @property
+    def shape(self):
+        return len(self.oidx), self.adatas[0].n_vars
 
-        self.obs_view.set_index(adatas_oidx, reverse, vidx)
-        self.obsm_view.set_index(adatas_oidx, reverse, vidx)
-        self.layers_view.set_index(adatas_oidx, reverse, vidx)
+    def __getitem__(self, index: Index):
+        oidx, vidx = _normalize_indices(index, self.obs_names, self.var_names)
+        resolved_idx = self._resolve_idx(oidx, vidx)
+
+        return AnnDataConcatView(
+            self.adatas,
+            self.limits,
+            resolved_idx,
+            self.obs_names[oidx],
+            self.var_names[vidx],
+        )
 
 
-class AnnDataConcatObs:
+class AnnDataConcatObs(_IndxViewMixin):
     def __init__(self, adatas, keys=None, index_unique=None):
         if isinstance(adatas, Mapping):
             if keys is not None:
@@ -125,30 +160,20 @@ class AnnDataConcatObs:
         for i in range(len(adatas) - 1):
             self.limits.append(self.limits[i] + adatas[i + 1].n_obs)
 
-        self.concat_view = None
+        self.obs = pd.DataFrame(index=self.obs_names)
 
     def __getitem__(self, index: Index):
-        reverse = None
-        adatas_oidx = []
-
         oidx, vidx = _normalize_indices(index, self.obs_names, self.var_names)
-        if isinstance(oidx, slice):
-            start, stop, step = oidx.indices(self.limits[-1])
-            u_oidx = np.arange(start, stop, step)
-        else:
-            if not np.all(np.diff(oidx) > 0):
-                u_oidx, reverse = np.unique(oidx, return_inverse=True)
-            else:
-                u_oidx = oidx
-        for lower, upper in zip([0] + self.limits, self.limits):
-            mask = (u_oidx >= lower) & (u_oidx < upper)
-            adatas_oidx.append(u_oidx[mask] - lower if mask.any() else None)
+        resolved_idx = self._resolve_idx(oidx, vidx)
 
-        if self.concat_view is None:
-            self.concat_view = AnnDataConcatView(
-                self.adatas, adatas_oidx, reverse, vidx
-            )
-        else:
-            self.concat_view.set_index(adatas_oidx, reverse, vidx)
+        return AnnDataConcatView(
+            self.adatas,
+            self.limits,
+            resolved_idx,
+            self.obs_names[oidx],
+            self.var_names[vidx],
+        )
 
-        return self.concat_view
+    @property
+    def shape(self):
+        return self.limits[-1], self.adatas[0].n_vars
