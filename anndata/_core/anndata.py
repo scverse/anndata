e8a116db565b54d0271798f6f34c641eed2b4c18
Robrecht Cannoodt <rcannood@gmail.com>
2020-11-30 16:24:42 +0100

    forgot to run black

--- anndata/_core/anndata.py
+++ anndata/_core/anndata.py
@@ -627,7 +627,9 @@ class AnnData(metaclass=utils.DeprecationMixinMeta):
     def X(self, value: Optional[Union[np.ndarray, sparse.spmatrix]]):
         if value is None:
             if self.isbacked:
-                raise ValueError("Cannot currently remove data matrix from backed object.")
+                raise ValueError(
+                    "Cannot currently remove data matrix from backed object."
+                )
             if self.is_view:
                 self._init_as_actual(self.copy())
             self._X = None
