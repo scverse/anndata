b5dae931fe229d965e99f148df61b845e8a1a312
Koncopd <koncopd@gmail.com>
2020-12-03 11:25:18 +0100

    revert switch to flit

--- anndata/__init__.py
+++ anndata/__init__.py
@@ -1,23 +1,21 @@
 """Annotated multivariate observation data."""
 
-from ._metadata import __version__, __author__, __email__, within_flit
+from ._metadata import __version__, __author__, __email__
 
-if not within_flit():
-    del within_flit
-    from ._core.anndata import AnnData, ImplicitModificationWarning
-    from ._core.merge import concat
-    from ._core.raw import Raw
-    from ._io import (
-        read_h5ad,
-        read_loom,
-        read_hdf,
-        read_excel,
-        read_umi_tools,
-        read_csv,
-        read_text,
-        read_mtx,
-        read_zarr,
-    )
+from ._core.anndata import AnnData, ImplicitModificationWarning
+from ._core.merge import concat
+from ._core.raw import Raw
+from ._io import (
+    read_h5ad,
+    read_loom,
+    read_hdf,
+    read_excel,
+    read_umi_tools,
+    read_csv,
+    read_text,
+    read_mtx,
+    read_zarr,
+)
 
-    # backwards compat / shortcut for default format
-    from ._io import read_h5ad as read
+# backwards compat / shortcut for default format
+from ._io import read_h5ad as read
