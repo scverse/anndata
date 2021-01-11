50062e4cec92defa5700779316852ec1df1bc47a
Gökçen Eraslan <gokcen.eraslan@gmail.com>
2021-01-11 10:17:43 -0500

    remove_unused_categories(inplace=True) is deprecated
    in pandas 1.2.0

--- anndata/_core/anndata.py
+++ anndata/_core/anndata.py
@@ -1113,7 +1113,7 @@ class AnnData(metaclass=utils.DeprecationMixinMeta):
             if not is_categorical_dtype(df_full[k]):
                 continue
             all_categories = df_full[k].cat.categories
-            df_sub[k].cat.remove_unused_categories(inplace=True)
+            df_sub[k] = df_sub[k].cat.remove_unused_categories()
             # also correct the colors...
             color_key = f"{k}_colors"
             if color_key not in uns:
