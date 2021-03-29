eecb5732f76cabd54d9a834cd4b9f741cda5eefd
Koncopd <koncopd@gmail.com>
2021-03-29 17:51:12 +0200

    proper handing of inner and outer joins

--- anndata/dev/multi_files/_anndataset.py
+++ anndata/dev/multi_files/_anndataset.py
@@ -4,8 +4,10 @@ import pandas as pd
 
 from ..._core.index import _normalize_indices, Index
 from ..._core.views import _resolve_idx
-from ..._core.merge import concat_arrays
+from ..._core.merge import concat_arrays, inner_concat_aligned_mapping
 from ...logging import anndata_logger as logger
+
+ATTRS = ["obs", "obsm", "layers"]
 
 
 def _merge(arrs):
@@ -45,14 +47,14 @@ class _ConcatViewMixin:
 class MapObsView:
     def __init__(self, attr, adatas, keys, adatas_oidx, reverse, vidx=None):
         self.adatas = adatas
-        self.keys = keys
+        self._keys = keys
         self.adatas_oidx = adatas_oidx
         self.reverse = reverse
         self.vidx = vidx
         self.attr = attr
 
     def __getitem__(self, key):
-        if key not in self.keys:
+        if self._keys is not None and key not in self._keys:
             raise KeyError(f"No {key} in {self.attr} view")
 
         arrs = []
@@ -62,35 +64,50 @@ class MapObsView:
                 idx = (oidx, self.vidx) if self.vidx is not None else oidx
                 arrs.append(arr[idx])
 
-        _arr = _merge(arrs)
+        _arr = _merge(arrs) if len(arrs) > 1 else arrs[0]
 
         return _arr[self.reverse] if self.reverse is not None else _arr
 
     def __repr__(self):
-        descr = f"View of {self.attr} with keys: {str(self.keys)[1:-1]}"
+        if self._keys is not None:
+            keys = self._keys
+        else:
+            keys = list(getattr(self.adatas[0], self.attr).keys())
+        descr = f"View of {self.attr} with keys: {str(keys)[1:-1]}"
         return descr
 
 
 class AnnDataConcatView(_ConcatViewMixin):
-    def __init__(self, adatas, limits, resolved_idx, obs_names, var_names, attrs_keys):
-        self.adatas = adatas
-        self.limits = limits
+    def __init__(self, reference, resolved_idx, obs_names, var_names):
+        self.reference = reference
+
+        self.adatas = self.reference.adatas
+        self.limits = self.reference.limits
+
         self.obs_names = obs_names
         self.var_names = var_names
 
         self.adatas_oidx, self.oidx, self.vidx, self.reverse = resolved_idx
 
-        for attr, keys in attrs_keys.items():
+        self._view_attrs_keys = self.reference._view_attrs_keys
+        self._attrs = self.reference._attrs
+
+        for attr, keys in self._view_attrs_keys.items():
             set_vidx = self.vidx if attr == "layers" else None
             setattr(
                 self,
                 attr + "_view",
                 MapObsView(
-                    attr, adatas, keys, self.adatas_oidx, self.reverse, set_vidx
+                    attr, self.adatas, keys, self.adatas_oidx, self.reverse, set_vidx
                 ),
             )
-
-        self._attrs_keys = attrs_keys
+        # non-view attrs
+        for attr in self._attrs:
+            setattr(
+                self,
+                attr + "_view",
+                MapObsView(attr, [self.reference], None, [self.oidx], None),
+            )
 
     @property
     def X(self):
@@ -124,25 +141,34 @@ class AnnDataConcatView(_ConcatViewMixin):
         resolved_idx = self._resolve_idx(oidx, vidx)
 
         return AnnDataConcatView(
-            self.adatas,
-            self.limits,
+            self.reference,
             resolved_idx,
             self.obs_names[oidx],
             self.var_names[vidx],
-            self._attrs_keys,
         )
 
     def __repr__(self):
         n_obs, n_vars = self.shape
         descr = f"AnnDataConcatView object with n_obs × n_vars = {n_obs} × {n_vars}"
-        for attr, keys in self._attrs_keys.items():
+        all_attrs_keys = self._view_attrs_keys.copy()
+        for attr in self._attrs:
+            all_attrs_keys[attr] = list(getattr(self.reference, attr).keys())
+        for attr, keys in all_attrs_keys.items():
             if len(keys) > 0:
                 descr += f"\n    {attr}: {str(keys)[1:-1]}"
         return descr
 
 
 class AnnDataConcatObs(_ConcatViewMixin):
-    def __init__(self, adatas, keys=None, index_unique=None):
+    def __init__(
+        self,
+        adatas,
+        join_obs="inner",
+        join_obsm=None,
+        label=None,
+        keys=None,
+        index_unique=None,
+    ):
         if isinstance(adatas, Mapping):
             if keys is not None:
                 raise TypeError(
@@ -172,14 +198,41 @@ class AnnDataConcatObs(_ConcatViewMixin):
         if not self.obs_names.is_unique:
             logger.info("Observation names are not unique.")
 
-        self._attrs_keys = {}
-        for attr in ["obs", "obsm", "layers"]:
-            self._attrs_keys[attr] = list(getattr(adatas[0], attr).keys())
+        view_attrs = ATTRS.copy()
+
+        self._attrs = []
+        # process obs joins
+        if join_obs is not None:
+            view_attrs.remove("obs")
+            self._attrs.append("obs")
+            concat_annot = pd.concat(
+                [a.obs for a in adatas], join=join_obs, ignore_index=True
+            )
+            concat_annot.index = self.obs_names
+            self.obs = concat_annot
+        else:
+            self.obs = pd.DataFrame(index=self.obs_names)
+        if label is not None:
+            self.obs[label] = label_col
+
+        # process obsm inner join
+        self.obsm = None
+        if join_obsm == "inner":
+            view_attrs.remove("obsm")
+            self._attrs.append("obsm")
+            self.obsm = inner_concat_aligned_mapping(
+                [a.obsm for a in adatas], index=self.obs_names
+            )
+
+        # process inner join of views
+        self._view_attrs_keys = {}
+        for attr in view_attrs:
+            self._view_attrs_keys[attr] = list(getattr(adatas[0], attr).keys())
 
         for a in adatas[1:]:
             if not adatas[0].var_names.equals(a.var_names):
                 raise ValueError("Variables in the adatas are different.")
-            for attr, keys in self._attrs_keys.items():
+            for attr, keys in self._view_attrs_keys.items():
                 ai_attr = getattr(a, attr)
                 a0_attr = getattr(adatas[0], attr)
                 new_keys = []
@@ -189,7 +242,7 @@ class AnnDataConcatObs(_ConcatViewMixin):
                         ai_ashape = ai_attr[key].shape
                         if len(a0_ashape) < 2 or a0_ashape[1] == ai_ashape[1]:
                             new_keys.append(key)
-                self._attrs_keys[attr] = new_keys
+                self._view_attrs_keys[attr] = new_keys
 
         self.var_names = adatas[0].var_names
 
@@ -199,19 +252,12 @@ class AnnDataConcatObs(_ConcatViewMixin):
         for i in range(len(adatas) - 1):
             self.limits.append(self.limits[i] + adatas[i + 1].n_obs)
 
-        self.obs = pd.DataFrame(index=self.obs_names)
-
     def __getitem__(self, index: Index):
         oidx, vidx = _normalize_indices(index, self.obs_names, self.var_names)
         resolved_idx = self._resolve_idx(oidx, vidx)
 
         return AnnDataConcatView(
-            self.adatas,
-            self.limits,
-            resolved_idx,
-            self.obs_names[oidx],
-            self.var_names[vidx],
-            self._attrs_keys,
+            self, resolved_idx, self.obs_names[oidx], self.var_names[vidx]
         )
 
     @property
@@ -221,12 +267,16 @@ class AnnDataConcatObs(_ConcatViewMixin):
     def __repr__(self):
         n_obs, n_vars = self.shape
         descr = f"AnnDataConcatObs object with n_obs × n_vars = {n_obs} × {n_vars}"
-        for attr, keys in self._attrs_keys.items():
+        for attr, keys in self._view_attrs_keys.items():
             if len(keys) > 0:
                 descr += f"\n    view of {attr}: {str(keys)[1:-1]}"
-
-        obs_keys = list(self.obs.keys())
-        if len(obs_keys) > 0:
-            descr += f"\n    obs: {str(obs_keys)[1:-1]}"
+        for attr in self._attrs:
+            keys = list(getattr(self, attr).keys())
+            if len(keys) > 0:
+                descr += f"\n    {attr}: {str(keys)[1:-1]}"
+        if "obs" in self._view_attrs_keys:
+            keys = list(self.obs.keys())
+            if len(keys) > 0:
+                descr += f"\n    own obs: {str(keys)[1:-1]}"
 
         return descr
