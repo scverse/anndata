f029f8e1ad71eec6e8dab6156168b98a308ce50b
Michal Klein <michal.klein@protonmail.com>
2021-12-30 00:58:51 +0100

    Add test

--- anndata/tests/test_views.py
+++ anndata/tests/test_views.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from operator import mul
 
 import joblib
@@ -8,6 +9,7 @@ import pytest
 
 import anndata as ad
 from anndata._core.index import _normalize_index
+from anndata._core.views import ArrayView, SparseCSRView
 from anndata.utils import asarray
 
 from anndata.tests.helpers import (
@@ -513,3 +515,18 @@ def test_negative_scalar_index(adata, index: int, obs: bool):
     np.testing.assert_array_equal(
         adata_pos_subset.var_names, adata_neg_subset.var_names
     )
+
+
+def test_deepcopy_subset(adata):
+    adata.obsp["arr"] = np.zeros((adata.n_obs, adata.n_obs))
+    adata.obsp["spmat"] = sparse.csr_matrix((adata.n_obs, adata.n_obs))
+
+    adata = deepcopy(adata[:10].copy())
+
+    assert isinstance(adata.obsp["arr"], np.ndarray)
+    assert not isinstance(adata.obsp["arr"], ArrayView)
+    np.testing.assert_array_equal(adata.obsp["arr"].shape, (10, 10))
+
+    assert sparse.isspmatrix_csr(adata.obsp["spmat"])
+    assert not isinstance(adata.obsp["spmat"], SparseCSRView)
+    np.testing.assert_array_equal(adata.obsp["spmat"].shape, (10, 10))
