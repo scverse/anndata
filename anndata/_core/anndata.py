df571ea46a8bb6ec158bf50acddbc828a1ac0b49
Isaac Virshup <ivirshup@gmail.com>
2022-01-12 17:07:59 +0100

    Special case copy for X=None

--- anndata/_core/anndata.py
+++ anndata/_core/anndata.py
@@ -1497,15 +1497,16 @@ class AnnData(metaclass=utils.DeprecationMixinMeta):
     def copy(self, filename: Optional[PathLike] = None) -> "AnnData":
         """Full copy, optionally on disk."""
         if not self.isbacked:
-            if self.is_view:
+            if self.is_view and self._has_X():
                 # TODO: How do I unambiguously check if this is a copy?
                 # Subsetting this way means we don’t have to have a view type
                 # defined for the matrix, which is needed for some of the
                 # current distributed backend. Specifically Dask.
-                X = _subset(self._adata_ref.X, (self._oidx, self._vidx)).copy()
+                return self._mutated_copy(
+                    X=_subset(self._adata_ref.X, (self._oidx, self._vidx)).copy()
+                )
             else:
-                X = self.X.copy()
-            return self._mutated_copy(X=X)
+                return self._mutated_copy()
         else:
             from .._io import read_h5ad
             from .._io.write import _write_h5ad
