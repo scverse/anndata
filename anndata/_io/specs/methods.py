e675901e80d03b40ae3d7885fd94fbfd9fda1ea3
MeeseeksMachine <39504233+meeseeksmachine@users.noreply.github.com>
2022-11-23 19:07:11 +0100

    Backport PR #856: Fix order type (#858)
    Co-authored-by: Isaac Virshup <ivirshup@gmail.com>

--- anndata/_io/specs/methods.py
+++ anndata/_io/specs/methods.py
@@ -623,7 +623,7 @@ def read_categorical(elem):
     return pd.Categorical.from_codes(
         codes=read_elem(elem["codes"]),
         categories=read_elem(elem["categories"]),
-        ordered=_read_attr(elem.attrs, "ordered"),
+        ordered=bool(_read_attr(elem.attrs, "ordered")),
     )
 
 
@@ -633,7 +633,7 @@ def read_categorical(elem, *, items=None, indices=(slice(None),)):
     return pd.Categorical.from_codes(
         codes=read_elem_partial(elem["codes"], indices=indices),
         categories=read_elem(elem["categories"]),
-        ordered=_read_attr(elem.attrs, "ordered"),
+        ordered=bool(_read_attr(elem.attrs, "ordered")),
     )
 
 
