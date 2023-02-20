9788989fc9f754d486ed62cc571283a00d92ac21
Isaac Virshup <ivirshup@gmail.com>
2023-02-20 17:42:51 +0100

    simplify test using read_dispatched

--- anndata/tests/test_io_dispatched.py
+++ anndata/tests/test_io_dispatched.py
@@ -17,9 +17,7 @@ from anndata.tests.helpers import gen_adata, assert_equal
 def test_read_dispatched_w_regex():
     def read_only_axis_dfs(func, elem_name: str, elem, iospec):
         if iospec.encoding_type == "anndata":
-            return ad.AnnData(
-                **{k: read_dispatched(v, read_only_axis_dfs) for k, v in elem.items()}
-            )
+            return func(elem)
         elif re.match(r"^/((obs)|(var))?(/.*)?$", elem_name):
             return func(elem)
         else:
