3df41c3f323a3163c32572d16c579d95e6b9c7cd
ilan-gold <ilanbassgold@gmail.com>
2023-03-10 13:32:51 +0100

    (chore): remove auto-generated typing

--- anndata/experimental/read_remote/read_remote.py
+++ anndata/experimental/read_remote/read_remote.py
@@ -69,7 +69,7 @@ class LazyCategoricalArray(ExplicitlyIndexedNDArrayMixin):
     def __repr__(self) -> str:
         return f"LazyCategoricalArray(codes=..., categories={self.categories}, ordered={self.ordered})"
 
-    def __eq__(self, __o: object) -> bool:
+    def __eq__(self, __o) -> bool:
         return self[()] == __o
 
 
