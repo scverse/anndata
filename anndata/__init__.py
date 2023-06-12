c025315f2c211d57f1607278f4a7d6ca5c92af40
Phil Schaf <flying-sheep@web.de>
2023-06-12 11:39:54 +0200

    Remove build hacks

--- anndata/__init__.py
+++ anndata/__init__.py
@@ -1,29 +1,27 @@
 """Annotated multivariate observation data."""
 
-from ._metadata import __version__, within_flit
+from ._version import __version__
 
-if not within_flit():
-    del within_flit
-    from ._core.anndata import AnnData
-    from ._core.merge import concat
-    from ._core.raw import Raw
-    from ._io import (
-        read_h5ad,
-        read_loom,
-        read_hdf,
-        read_excel,
-        read_umi_tools,
-        read_csv,
-        read_text,
-        read_mtx,
-        read_zarr,
-    )
-    from ._warnings import (
-        OldFormatWarning,
-        WriteWarning,
-        ImplicitModificationWarning,
-        ExperimentalFeatureWarning,
-    )
+from ._core.anndata import AnnData
+from ._core.merge import concat
+from ._core.raw import Raw
+from ._io import (
+    read_h5ad,
+    read_loom,
+    read_hdf,
+    read_excel,
+    read_umi_tools,
+    read_csv,
+    read_text,
+    read_mtx,
+    read_zarr,
+)
+from ._warnings import (
+    OldFormatWarning,
+    WriteWarning,
+    ImplicitModificationWarning,
+    ExperimentalFeatureWarning,
+)
 
-    # backwards compat / shortcut for default format
-    from ._io import read_h5ad as read
+# backwards compat / shortcut for default format
+from ._io import read_h5ad as read
