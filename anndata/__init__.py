35ea7c35bc3cd692e96c5629108cf4d4feb89bf5
Phil Schaf <flying-sheep@web.de>
2023-06-15 18:01:37 +0200

    re-add dynamic version

--- anndata/__init__.py
+++ anndata/__init__.py
@@ -1,6 +1,16 @@
 """Annotated multivariate observation data."""
 
-from ._version import __version__
+try:  # See https://github.com/maresb/hatch-vcs-footgun-example
+    from setuptools_scm import get_version
+
+    __version__ = get_version(root="..", relative_to=__file__)
+except (ImportError, LookupError):
+    try:
+        from ._version import __version__
+    except ModuleNotFoundError:
+        raise RuntimeError(
+            "anndata is not correctly installed. Please install it, e.g. with pip."
+        )
 
 from ._core.anndata import AnnData
 from ._core.merge import concat
