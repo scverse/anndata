d39c5d3e1d7855733a3edf288b68ef1c8d39aee8
Isaac Virshup <ivirshup@gmail.com>
2023-07-28 13:01:17 +0000

    Simplify pytest setup

--- conftest.py
+++ conftest.py
@@ -3,23 +3,13 @@
 import pytest
 
 
-def pytest_addoption(parser):
-    parser.addoption(
-        "--only-gpu",
-        action="store_true",
-        default=False,
-        help="Only run GPU tests.",
-    )
+def pytest_itemcollected(item):
+    # Defniing behavior of pytest.mark.gpu
+    from importlib.util import find_spec
 
-
-def pytest_runtest_setup(item):
     gpu = len([mark for mark in item.iter_markers(name="gpu")]) > 0
 
-    if not gpu:  # Skip non gpu tests on gpu CI
-        if item.config.getoption("--only-gpu"):
-            pytest.skip("Only running GPU tests.")
-    else:  # Skip GPU tests if cupy not installed
-        from importlib.util import find_spec
-
-        if not find_spec("cupy"):
-            pytest.skip("Cupy not installed.")
+    if gpu:
+        item.add_marker(
+            pytest.mark.skipif(not find_spec("cupy"), reason="Cupy not installed.")
+        )
