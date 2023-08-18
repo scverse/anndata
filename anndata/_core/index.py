48a0961cb39f01b3b79e12f795ffc40786d0c8c4
Philipp A <flying-sheep@web.de>
2023-08-18 15:37:56 +0200

    more careful

--- anndata/_core/index.py
+++ anndata/_core/index.py
@@ -142,7 +142,7 @@ def _subset_spmatrix(a: spmatrix, subset_idx: Index):
 
 @_subset.register(pd.DataFrame)
 def _subset_df(df: pd.DataFrame, subset_idx: Index):
-    if len(subset_idx) == 1:
+    if isinstance(subset_idx, tuple) and len(subset_idx) == 1:
         return df.iloc[subset_idx[0]]
     return df.iloc[subset_idx]
 
