ae707729ed1585345c3b0dc52893df3f2417e6d8
Philipp A <flying-sheep@web.de>
2023-08-21 13:45:29 +0200

    Discard changes to anndata/_core/index.py

--- anndata/_core/index.py
+++ anndata/_core/index.py
@@ -142,8 +142,6 @@ def _subset_spmatrix(a: spmatrix, subset_idx: Index):
 
 @_subset.register(pd.DataFrame)
 def _subset_df(df: pd.DataFrame, subset_idx: Index):
-    if isinstance(subset_idx, tuple) and len(subset_idx) == 1:
-        return df.iloc[subset_idx[0]]
     return df.iloc[subset_idx]
 
 
