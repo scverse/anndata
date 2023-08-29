5a8b5ecc78ba4efc0f1edcf7694cc987553a1cc1
Intron7 <severin.dicks@icloud.com>
2023-08-29 12:33:45 +0200

    added raw GPU test

--- anndata/tests/test_gpu.py
+++ anndata/tests/test_gpu.py
@@ -1,5 +1,6 @@
 import pytest
-
+from anndata import AnnData, Raw
+from scipy.sparse import isspmatrix_csr
 
 @pytest.mark.gpu
 def test_gpu():
@@ -9,3 +10,19 @@ def test_gpu():
     import cupy  # This test shouldn't run if cupy isn't installed
 
     cupy.ones(1)
+
+@pytest.mark.gpu
+def test_adata_raw_gpu():
+    from cupyx.scipy.sparse import random
+    import cupy as cp
+    adata = AnnData(X = random(500,50,density=0.01, format='csr', dtype=cp.float32))
+    adata.raw = adata
+    assert isspmatrix_csr(adata.raw.X)
+
+@pytest.mark.gpu
+def test_raw_gpu():
+    from cupyx.scipy.sparse import random
+    import cupy as cp
+    adata = AnnData(X = random(500,50,density=0.01, format='csr', dtype=cp.float32))
+    araw = Raw(adata)
+    assert isspmatrix_csr(araw.X)
