3047172dc6ee485a9eeb2f7f347e69c8459922c1
Isaac Virshup <ivirshup@gmail.com>
2023-08-30 17:07:12 +0200

    asarray for dask w sparse chunks

--- anndata/utils.py
+++ anndata/utils.py
@@ -9,7 +9,7 @@ from scipy import sparse
 
 from .logging import get_logger
 from ._core.sparse_dataset import SparseDataset
-from .compat import CupyArray, CupySparseMatrix
+from .compat import CupyArray, CupySparseMatrix, DaskArray
 
 logger = get_logger(__name__)
 
@@ -43,6 +43,11 @@ def asarray_cupy(x):
 @asarray.register(CupySparseMatrix)
 def asarray_cupy_sparse(x):
     return x.toarray().get()
+
+
+@asarray.register(DaskArray)
+def asarray_dask(x):
+    return asarray(x.compute())
 
 
 @singledispatch
