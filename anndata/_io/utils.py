19f39e6ec2d77f46966deb53db7369c9cc527983
Philipp A. <flying-sheep@web.de>
2023-10-10 12:56:19 +0200

    typo

--- anndata/_io/utils.py
+++ anndata/_io/utils.py
@@ -213,7 +213,7 @@ def report_read_key_on_error(func):
 
 def report_write_key_on_error(func):
     """\
-    A decorator for hdf6/zarr element writing which makes keys involved in errors get reported.
+    A decorator for hdf5/zarr element writing which makes keys involved in errors get reported.
 
     Example
     -------
