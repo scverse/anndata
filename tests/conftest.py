cd5b5300312dbc9f319e61fd68363a4d04617d5c
Philipp A. <flying-sheep@web.de>
2024-04-09 14:26:06 +0200

    clean up warnings

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,8 @@
 from __future__ import annotations
 
-import warnings
-
 import pytest
 
-import anndata
 from anndata.tests.helpers import subset_func  # noqa: F401
-
-# TODO: Should be done in pyproject.toml, see anndata/conftest.py
-warnings.filterwarnings("ignore", category=anndata.OldFormatWarning)
 
 
 @pytest.fixture
