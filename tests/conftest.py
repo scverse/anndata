bea8bff4af88ef86185f191933a9c18c8918bbff
Philipp A. <flying-sheep@web.de>
2024-04-09 14:00:47 +0200

    Move tests out of package

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,9 +10,6 @@ from anndata.tests.helpers import subset_func  # noqa: F401
 # TODO: Should be done in pyproject.toml, see anndata/conftest.py
 warnings.filterwarnings("ignore", category=anndata.OldFormatWarning)
 
-# TODO: remove once we extricated test utils and tests
-collect_ignore = ["helpers.py"]
-
 
 @pytest.fixture
 def backing_h5ad(tmp_path):
