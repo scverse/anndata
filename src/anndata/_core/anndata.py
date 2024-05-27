4686bed487090a39a716f25cbf57c7a05bf32fef
ilan-gold <ilanbassgold@gmail.com>
2024-05-27 13:57:18 +0200

    (fix): don't change `pandas` function

--- src/anndata/_core/anndata.py
+++ src/anndata/_core/anndata.py
@@ -1101,7 +1101,7 @@ class AnnData(metaclass=utils.DeprecationMixinMeta):
                 continue
             all_categories = df_full[k].cat.categories
             with pd.option_context("mode.chained_assignment", None):
-                df_sub[k] = df_sub[k].cat.should_remove_unused_categories()
+                df_sub[k] = df_sub[k].cat.remove_unused_categories()
             # also correct the colors...
             color_key = f"{k}_colors"
             if color_key not in uns:
