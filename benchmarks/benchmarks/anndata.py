38d218fc74caa3d0b892324454331b6034bc2546
ilan-gold <ilanbassgold@gmail.com>
2024-06-03 16:41:13 +0200

    (fix): change name

--- benchmarks/benchmarks/anndata.py
+++ benchmarks/benchmarks/anndata.py
@@ -4,6 +4,6 @@ from .utils import gen_adata
 
 
 class GarbargeCollectionSuite:
-    def track_peakmem_write_compressed(self, *_):
+    def track_peakmem_garbage_collection(self, *_):
         for _ in range(50):
             adata = gen_adata(10000, 10000, "X-csc")  # noqa: F841
