a9eba7aa1943b125ad0a3a4669a60315832469b4
ilan-gold <ilanbassgold@gmail.com>
2024-06-03 16:42:27 +0200

    (chore): fewer runs

--- benchmarks/benchmarks/anndata.py
+++ benchmarks/benchmarks/anndata.py
@@ -5,5 +5,5 @@ from .utils import gen_adata
 
 class GarbargeCollectionSuite:
     def track_peakmem_garbage_collection(self, *_):
-        for _ in range(50):
+        for _ in range(10):
             adata = gen_adata(10000, 10000, "X-csc")  # noqa: F841
