2df03b51685a1f51fb97710eb768a0cc04ac75f9
ilan-gold <ilanbassgold@gmail.com>
2024-06-03 16:37:28 +0200

    (fix): use right `gen_adata` function

--- benchmarks/benchmarks/anndata.py
+++ benchmarks/benchmarks/anndata.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
-from anndata.tests.helpers import gen_adata
+from .utils import gen_adata
 
 
 class GarbargeCollectionSuite:
     def track_peakmem_write_compressed(self, *_):
         for _ in range(50):
-            adata = gen_adata((10000, 10000))  # noqa: F841
+            adata = gen_adata(10000, 10000, "X-csc")  # noqa: F841
