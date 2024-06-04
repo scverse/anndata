6ed99634ceabd810203bde22606adcad2db1e230
ilan-gold <ilanbassgold@gmail.com>
2024-06-04 14:08:35 +0200

    (fix): try `track_peakmem`

--- benchmarks/benchmarks/anndata.py
+++ benchmarks/benchmarks/anndata.py
@@ -1,9 +1,13 @@
 from __future__ import annotations
 
-from .utils import gen_adata
+from .utils import gen_adata, get_peak_mem, sedate
+
+
+def peakmem_garbage_collection():
+    for _ in range(10):
+        adata = gen_adata(10000, 10000, "X-csc")  # noqa: F841
 
 
 class GarbargeCollectionSuite:
-    def peakmem_garbage_collection(self, *_):
-        for _ in range(10):
-            adata = gen_adata(10000, 10000, "X-csc")  # noqa: F841
+    def track_peakmem_write_compressed(self, *_):
+        return get_peak_mem((sedate(peakmem_garbage_collection), (), {}))
