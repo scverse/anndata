c971dfbc318bbf06b49dd1b014ff17889bcd98ed
ilan-gold <ilanbassgold@gmail.com>
2024-06-05 15:29:35 +0200

    (fix): do custom peakmem track

--- benchmarks/benchmarks/anndata.py
+++ benchmarks/benchmarks/anndata.py
@@ -1,13 +1,33 @@
 from __future__ import annotations
 
-from .utils import gen_adata, get_peak_mem, sedate
+import tracemalloc
 
+import numpy as np
 
-def run_garbage_collection():
-    for _ in range(10):
-        adata = gen_adata(10000, 10000, "X-csc")  # noqa: F841
+from .utils import gen_adata
 
 
 class GarbargeCollectionSuite:
+    runs = 10
+
+    # https://github.com/pythonprofilers/memory_profiler/issues/402 and other backend does not pick this up
     def track_peakmem_write_compressed(self, *_):
-        return get_peak_mem((sedate(run_garbage_collection), (), {}))
+        def display_top(snapshot, key_type="lineno"):
+            snapshot = snapshot.filter_traces(
+                (
+                    tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
+                    tracemalloc.Filter(False, "<unknown>"),
+                )
+            )
+            top_stats = snapshot.statistics(key_type)
+            total = sum(stat.size for stat in top_stats)
+            return total
+
+        total = np.zeros(self.n)
+        tracemalloc.start()
+        for i in range(self.n):
+            data = gen_adata(10000, 10000, "X-csc")  # noqa: F841
+            snapshot = tracemalloc.take_snapshot()
+            total[i] = display_top(snapshot)
+        tracemalloc.stop()
+        return max(total)
