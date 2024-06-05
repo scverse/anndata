ce4e2d877e5e6d620496c7adecc8e8842eace07f
ilan-gold <ilanbassgold@gmail.com>
2024-06-05 14:14:51 +0200

    (fix): remove `track_` name

--- benchmarks/benchmarks/anndata.py
+++ benchmarks/benchmarks/anndata.py
@@ -3,11 +3,11 @@ from __future__ import annotations
 from .utils import gen_adata, get_peak_mem, sedate
 
 
-def peakmem_garbage_collection():
+def run_garbage_collection():
     for _ in range(10):
         adata = gen_adata(10000, 10000, "X-csc")  # noqa: F841
 
 
 class GarbargeCollectionSuite:
     def track_peakmem_write_compressed(self, *_):
-        return get_peak_mem((sedate(peakmem_garbage_collection), (), {}))
+        return get_peak_mem((sedate(run_garbage_collection), (), {}))
