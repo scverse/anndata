2ffc41ba9233834cc8cac9ae47f57202deb4b607
Philipp A <flying-sheep@web.de>
2024-06-27 14:00:55 +0200

    Backport PR #1537: Fix assigning and initializing with h5py datasets

--- src/anndata/tests/helpers.py
+++ src/anndata/tests/helpers.py
@@ -25,6 +25,7 @@ from anndata.compat import (
     CupyCSRMatrix,
     CupySparseMatrix,
     DaskArray,
+    ZarrArray,
 )
 from anndata.utils import asarray
 
@@ -413,7 +414,7 @@ def assert_equal_ndarray(a, b, exact=False, elem_name=None):
     b = asarray(b)
     if not exact and is_numeric_dtype(a) and is_numeric_dtype(b):
         assert a.shape == b.shape, format_msg(elem_name)
-        assert np.allclose(a, b, equal_nan=True), format_msg(elem_name)
+        np.testing.assert_allclose(a, b, equal_nan=True, err_msg=format_msg(elem_name))
     elif (  # Structured dtype
         not exact
         and hasattr(a, "dtype")
@@ -449,6 +450,7 @@ def assert_equal_cupy_sparse(a, b, exact=False, elem_name=None):
 
 
 @assert_equal.register(h5py.Dataset)
+@assert_equal.register(ZarrArray)
 def assert_equal_h5py_dataset(a, b, exact=False, elem_name=None):
     a = asarray(a)
     assert_equal(b, a, exact, elem_name=elem_name)
