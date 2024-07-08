b2bdd7f926d54c9ae7a1b56a4e97d37e6e4d1dad
Philipp A <flying-sheep@web.de>
2024-07-08 09:18:08 +0200

    Cleanup types (#1559)

--- src/anndata/_core/index.py
+++ src/anndata/_core/index.py
@@ -1,16 +1,19 @@
 from __future__ import annotations
 
-import collections.abc as cabc
-from collections.abc import Sequence
+from collections.abc import Iterable, Sequence
 from functools import singledispatch
 from itertools import repeat
+from typing import TYPE_CHECKING
 
 import h5py
 import numpy as np
 import pandas as pd
 from scipy.sparse import issparse, spmatrix
 
-from ..compat import AwkArray, DaskArray, Index, Index1D, SpArray
+from ..compat import AwkArray, DaskArray, SpArray
+
+if TYPE_CHECKING:
+    from ..compat import Index, Index1D
 
 
 def _normalize_indices(
@@ -142,14 +145,14 @@ def unpack_index(index: Index) -> tuple[Index1D, Index1D]:
 def _subset(a: np.ndarray | pd.DataFrame, subset_idx: Index):
     # Select as combination of indexes, not coordinates
     # Correcting for indexing behaviour of np.ndarray
-    if all(isinstance(x, cabc.Iterable) for x in subset_idx):
+    if all(isinstance(x, Iterable) for x in subset_idx):
         subset_idx = np.ix_(*subset_idx)
     return a[subset_idx]
 
 
 @_subset.register(DaskArray)
 def _subset_dask(a: DaskArray, subset_idx: Index):
-    if len(subset_idx) > 1 and all(isinstance(x, cabc.Iterable) for x in subset_idx):
+    if len(subset_idx) > 1 and all(isinstance(x, Iterable) for x in subset_idx):
         if issparse(a._meta) and a._meta.format == "csc":
             return a[:, subset_idx[1]][subset_idx[0], :]
         return a[subset_idx[0], :][:, subset_idx[1]]
@@ -160,7 +163,7 @@ def _subset_dask(a: DaskArray, subset_idx: Index):
 @_subset.register(SpArray)
 def _subset_sparse(a: spmatrix | SpArray, subset_idx: Index):
     # Correcting for indexing behaviour of sparse.spmatrix
-    if len(subset_idx) > 1 and all(isinstance(x, cabc.Iterable) for x in subset_idx):
+    if len(subset_idx) > 1 and all(isinstance(x, Iterable) for x in subset_idx):
         first_idx = subset_idx[0]
         if issubclass(first_idx.dtype.type, np.bool_):
             first_idx = np.where(first_idx)[0]
@@ -175,7 +178,7 @@ def _subset_df(df: pd.DataFrame, subset_idx: Index):
 
 @_subset.register(AwkArray)
 def _subset_awkarray(a: AwkArray, subset_idx: Index):
-    if all(isinstance(x, cabc.Iterable) for x in subset_idx):
+    if all(isinstance(x, Iterable) for x in subset_idx):
         subset_idx = np.ix_(*subset_idx)
     return a[subset_idx]
 
