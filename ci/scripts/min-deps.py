076b92fd7e2fe818c825e14d33e069e5e0dd8db9
ilan-gold <ilanbassgold@gmail.com>
2024-08-06 11:00:58 -0400

    (fix): use `test-full`/`test`

--- ci/scripts/min-deps.py
+++ ci/scripts/min-deps.py
@@ -62,7 +62,7 @@ def extract_min_deps(
             for extra in req.extras:
                 extra_deps = pyproject["project"]["optional-dependencies"][extra]
                 dependencies += map(Requirement, extra_deps)
-        elif req.name != "xarray":
+        else:
             yield min_dep(req)
 
 
