c4eee9bb631fb11826076d5f5beee43d2a3a1df3
Ilan Gold <ilanbassgold@gmail.com>
2024-08-26 07:24:43 -0400

    Backport PR #1589: (fix): disallow using dataframes with multi index columns

--- src/anndata/_core/anndata.py
+++ src/anndata/_core/anndata.py
@@ -29,7 +29,12 @@ from ..compat import (
     _move_adj_mtx,
 )
 from ..logging import anndata_logger as logger
-from ..utils import deprecated, dim_len, ensure_df_homogeneous
+from ..utils import (
+    deprecated,
+    dim_len,
+    ensure_df_homogeneous,
+    raise_value_error_if_multiindex_columns,
+)
 from .access import ElementRef
 from .aligned_df import _gen_dataframe
 from .aligned_mapping import AlignedMappingProperty, AxisArrays, Layers, PairwiseArrays
@@ -234,9 +239,13 @@ class AnnData(metaclass=utils.DeprecationMixinMeta):
         *,
         obsp: np.ndarray | Mapping[str, Sequence[Any]] | None = None,
         varp: np.ndarray | Mapping[str, Sequence[Any]] | None = None,
-        oidx: Index1D = None,
-        vidx: Index1D = None,
+        oidx: Index1D | None = None,
+        vidx: Index1D | None = None,
     ):
+        # check for any multi-indices that aren’t later checked in coerce_array
+        for attr, key in [(obs, "obs"), (var, "var"), (X, "X")]:
+            if isinstance(attr, pd.DataFrame):
+                raise_value_error_if_multiindex_columns(attr, key)
         if asview:
             if not isinstance(X, AnnData):
                 raise ValueError("`X` has to be an AnnData object.")
@@ -731,9 +740,10 @@ class AnnData(metaclass=utils.DeprecationMixinMeta):
         """Number of variables/features."""
         return len(self.var_names)
 
-    def _set_dim_df(self, value: pd.DataFrame, attr: str):
+    def _set_dim_df(self, value: pd.DataFrame, attr: Literal["obs", "var"]):
         if not isinstance(value, pd.DataFrame):
             raise ValueError(f"Can only assign pd.DataFrame to {attr}.")
+        raise_value_error_if_multiindex_columns(value, attr)
         value_idx = self._prep_dim_index(value.index, attr)
         if self.is_view:
             self._init_as_actual(self.copy())
--- tests/test_base.py
+++ tests/test_base.py
@@ -12,7 +12,7 @@ from scipy import sparse as sp
 from scipy.sparse import csr_matrix, issparse
 
 from anndata import AnnData, ImplicitModificationWarning
-from anndata.tests.helpers import assert_equal, gen_adata
+from anndata.tests.helpers import assert_equal, gen_adata, get_multiindex_columns_df
 
 # some test objects that we use below
 adata_dense = AnnData(np.array([[1, 2], [3, 4]]))
@@ -111,6 +111,14 @@ def test_create_from_df():
     assert df.values.tolist() == ad.X.tolist()
     assert df.columns.tolist() == ad.var_names.tolist()
     assert df.index.tolist() == ad.obs_names.tolist()
+
+
+@pytest.mark.parametrize("attr", ["X", "obs", "obsm"])
+def test_error_create_from_multiindex_df(attr):
+    df = get_multiindex_columns_df((100, 20))
+    val = df if attr != "obsm" else {"df": df}
+    with pytest.raises(ValueError, match=r"MultiIndex columns are not supported"):
+        AnnData(**{attr: val}, shape=(100, 10))
 
 
 def test_create_from_sparse_df():
