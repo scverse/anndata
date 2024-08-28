d3401b2e5c649f23f9fbb1857c6a847b4df71bcd
ilan-gold <ilanbassgold@gmail.com>
2024-08-28 16:16:13 +0200

    (chore): clean up tests

--- tests/test_read_backed_experimental.py
+++ tests/test_read_backed_experimental.py
@@ -22,30 +22,31 @@ from anndata.tests.helpers import (
 @pytest.fixture(
     params=[sparse.csr_matrix, sparse.csc_matrix, np.array, as_dense_dask_array],
     ids=["scipy-csr", "scipy-csc", "np-array", "dask_array"],
+    scope="session",
 )
 def mtx_format(request):
     return request.param
 
 
-@pytest.fixture(params=[sparse.csr_matrix, sparse.csc_matrix])
-def sparse_format(request):
-    return request.param
-
-
-@pytest.fixture(params=["zarr", "h5ad"])
+@pytest.fixture(params=["zarr", "h5ad"], scope="session")
 def dskfmt(request):
     return request.param
 
 
-needs_xarray = pytest.mark.skipif(
-    not find_spec("xarray"), reason="Xarray is not installed"
-)
+@pytest.fixture(scope="session")
+def adata_remote_orig(
+    tmp_path_factory, dskfmt: str, mtx_format
+) -> tuple[AnnData, AnnData]:
+    orig_path = Path(tmp_path_factory.mktemp(f"orig.{dskfmt}"))
+    orig = gen_adata((1000, 1000), mtx_format)
+    orig.write_zarr(orig_path)
+    remote = read_backed(orig_path)
+    return remote, orig
 
 
-@needs_xarray
-def test_access_count_obs_var(tmp_path, mtx_format):
-    base_pth = Path(tmp_path)
-    orig_pth = base_pth / "orig.zarr"
+@pytest.fixture
+def adata_remote_with_store_tall_skinny(tmp_path_factory, mtx_format):
+    orig_path = Path(tmp_path_factory.mktemp("orig.zarr"))
     M = 1000000  # forces zarr to chunk `obs` columns multiple ways - that way 1 access to `int64` below is actually only one access
     N = 5
     obs_names = pd.Index(f"cell{i}" for i in range(M))
@@ -57,9 +58,20 @@ def test_access_count_obs_var(tmp_path, mtx_format):
         var=var,
         X=mtx_format(np.random.binomial(100, 0.005, (M, N)).astype(np.float32)),
     )
-    orig.write_zarr(orig_pth)
-    store = AccessTrackingStore(orig_pth)
+    orig.write_zarr(orig_path)
+    store = AccessTrackingStore(orig_path)
     remote = read_backed(store)
+    return remote, store
+
+
+needs_xarray = pytest.mark.skipif(
+    not find_spec("xarray"), reason="Xarray is not installed"
+)
+
+
+@needs_xarray
+def test_access_count_obs_var(adata_remote_with_store_tall_skinny):
+    remote, store = adata_remote_with_store_tall_skinny
     store.initialize_key_trackers(
         ["obs/cat/codes", "obs/cat/categories", "obs/int64", "var/int64", "X"]
     )
@@ -76,13 +88,6 @@ def test_access_count_obs_var(tmp_path, mtx_format):
     )
     assert (
         store.get_access_count("obs/cat/categories") == 0
-    ), store.get_subkeys_accessed("obs/cat/categories")
-    # This should only cause categories to be read in once
-    remote.obs["cat"].dtype
-    remote.obs["cat"].dtype
-    remote.obs["cat"].dtype
-    assert (
-        store.get_access_count("obs/cat/categories") == 1
     ), store.get_subkeys_accessed("obs/cat/categories")
     subset = remote[
         (remote.obs["cat"] == "a").data, :
@@ -112,42 +117,43 @@ def test_access_count_obs_var(tmp_path, mtx_format):
 
 
 @needs_xarray
-def test_to_memory(tmp_path, mtx_format, dskfmt):
-    adata = gen_adata((1000, 1000), mtx_format)
-    base_pth = Path(tmp_path)
-    orig_pth = base_pth / f"orig.{dskfmt}"
-    write = lambda x: getattr(x, f"write_{dskfmt}")(orig_pth)
-    write(adata)
-    remote = read_backed(orig_pth)
+def test_access_count_dtype(adata_remote_with_store_tall_skinny):
+    remote, store = adata_remote_with_store_tall_skinny
+    store.initialize_key_trackers(["obs/cat/categories"])
+    assert (
+        store.get_access_count("obs/cat/categories") == 0
+    ), store.get_subkeys_accessed("obs/cat/categories")
+    # This should only cause categories to be read in once
+    remote.obs["cat"].dtype
+    remote.obs["cat"].dtype
+    remote.obs["cat"].dtype
+    assert (
+        store.get_access_count("obs/cat/categories") == 1
+    ), store.get_subkeys_accessed("obs/cat/categories")
+
+
+@needs_xarray
+def test_to_memory(adata_remote_orig):
+    remote, orig = adata_remote_orig
     remote_to_memory = remote.to_memory()
-    assert_equal(remote_to_memory, adata)
+    assert_equal(remote_to_memory, orig)
 
 
 @needs_xarray
-def test_view_to_memory(tmp_path, mtx_format, dskfmt):
-    adata = gen_adata((1000, 1000), mtx_format)
-    base_pth = Path(tmp_path)
-    orig_pth = base_pth / f"orig.{dskfmt}"
-    write = lambda x: getattr(x, f"write_{dskfmt}")(orig_pth)
-    write(adata)
-    remote = read_backed(orig_pth)
-    subset_obs = adata.obs["obs_cat"] == "a"
-    assert_equal(adata[subset_obs, :], remote[subset_obs, :].to_memory())
+def test_view_to_memory(adata_remote_orig):
+    remote, orig = adata_remote_orig
+    subset_obs = orig.obs["obs_cat"] == "a"
+    assert_equal(orig[subset_obs, :], remote[subset_obs, :].to_memory())
 
-    subset_var = adata.var["var_cat"] == "a"
-    assert_equal(adata[:, subset_var], remote[:, subset_var].to_memory())
+    subset_var = orig.var["var_cat"] == "a"
+    assert_equal(orig[:, subset_var], remote[:, subset_var].to_memory())
 
 
 @needs_xarray
-def test_view_of_view_to_memory(tmp_path, mtx_format, dskfmt):
-    adata = gen_adata((1000, 1000), mtx_format)
-    base_pth = Path(tmp_path)
-    orig_pth = base_pth / f"orig.{dskfmt}"
-    write = lambda x: getattr(x, f"write_{dskfmt}")(orig_pth)
-    write(adata)
-    remote = read_backed(orig_pth)
-    subset_obs = (adata.obs["obs_cat"] == "a") | (adata.obs["obs_cat"] == "b")
-    subsetted_adata = adata[subset_obs, :]
+def test_view_of_view_to_memory(adata_remote_orig):
+    remote, orig = adata_remote_orig
+    subset_obs = (orig.obs["obs_cat"] == "a") | (orig.obs["obs_cat"] == "b")
+    subsetted_adata = orig[subset_obs, :]
     subset_subset_obs = subsetted_adata.obs["obs_cat"] == "b"
     subsetted_subsetted_adata = subsetted_adata[subset_subset_obs, :]
     assert_equal(
@@ -155,8 +161,8 @@ def test_view_of_view_to_memory(tmp_path, mtx_format, dskfmt):
         remote[subset_obs, :][subset_subset_obs, :].to_memory(),
     )
 
-    subset_var = (adata.var["var_cat"] == "a") | (adata.var["var_cat"] == "b")
-    subsetted_adata = adata[:, subset_var]
+    subset_var = (orig.var["var_cat"] == "a") | (orig.var["var_cat"] == "b")
+    subsetted_adata = orig[:, subset_var]
     subset_subset_var = subsetted_adata.var["var_cat"] == "b"
     subsetted_subsetted_adata = subsetted_adata[:, subset_subset_var]
     assert_equal(
