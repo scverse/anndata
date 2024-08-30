b332f043e13ae756aa0a65fcb38e37217c077447
ilan-gold <ilanbassgold@gmail.com>
2024-08-30 13:04:55 +0200

    (fix): split `__deprecated__` from `__all__`

--- src/anndata/experimental/__init__.py
+++ src/anndata/experimental/__init__.py
@@ -3,8 +3,17 @@ from __future__ import annotations
 import warnings
 from typing import TYPE_CHECKING
 
-from anndata._core.sparse_dataset import CSCDataset, CSRDataset, sparse_dataset
-from anndata._io.specs import IOSpec, read_elem, read_elem_as_dask, write_elem
+from anndata._core.sparse_dataset import (  # noqa: F401
+    CSCDataset,
+    CSRDataset,
+    sparse_dataset,
+)
+from anndata._io.specs import (  # noqa: F401
+    IOSpec,
+    read_elem,
+    read_elem_as_dask,
+    write_elem,
+)
 
 from .._types import Read, ReadCallback, StorageType, Write, WriteCallback
 from ._dispatch_io import read_dispatched, write_dispatched
@@ -15,11 +24,11 @@ from .pytorch import AnnLoader
 if TYPE_CHECKING:
     from typing import Any
 
-deprecated = ["CSRDataset", "CSCDataset", "read_elem", "write_elem"]
+__deprecated__ = ["CSRDataset", "CSCDataset", "read_elem", "write_elem"]
 
 
 def __getattr__(key: str) -> Any:
-    if key in deprecated:
+    if key in __deprecated__:
         msg = f"Importing {key} from `anndata.experimental` is deprecated. Import from `anndata` directly."
         warnings.warn(msg, FutureWarning)
     return globals()[key]
@@ -28,19 +37,19 @@ def __getattr__(key: str) -> Any:
 __all__ = [
     "AnnCollection",
     "AnnLoader",
-    "read_elem",
-    "write_elem",
     "read_elem_as_dask",
     "read_dispatched",
     "write_dispatched",
     "IOSpec",
     "concat_on_disk",
     "sparse_dataset",
-    "CSRDataset",
-    "CSCDataset",
     "Read",
     "Write",
     "ReadCallback",
     "WriteCallback",
     "StorageType",
 ]
+
+
+def __dir__():
+    return sorted(__all__ + __deprecated__)
