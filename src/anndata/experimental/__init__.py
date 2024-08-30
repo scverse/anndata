5013a5c42340603ce9cd86ca9fec799edfcdc069
ilan-gold <ilanbassgold@gmail.com>
2024-08-30 12:59:52 +0200

    (fix): dont blanket warn

--- src/anndata/experimental/__init__.py
+++ src/anndata/experimental/__init__.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+import warnings
+from typing import TYPE_CHECKING
+
 from anndata._core.sparse_dataset import CSCDataset, CSRDataset, sparse_dataset
 from anndata._io.specs import IOSpec, read_elem, read_elem_as_dask, write_elem
 
@@ -8,6 +11,19 @@ from ._dispatch_io import read_dispatched, write_dispatched
 from .merge import concat_on_disk
 from .multi_files import AnnCollection
 from .pytorch import AnnLoader
+
+if TYPE_CHECKING:
+    from typing import Any
+
+deprecated = ["CSRDataset", "CSCDataset", "read_elem", "write_elem"]
+
+
+def __getattr__(key: str) -> Any:
+    if key in deprecated:
+        msg = f"Importing {key} from `anndata.experimental` is deprecated. Import from `anndata` directly."
+        warnings.warn(msg, FutureWarning)
+    return globals()[key]
+
 
 __all__ = [
     "AnnCollection",
