c4b1bbc734a438d56afbc2ce7a68e2c00c71f2ca
ilan-gold <ilanbassgold@gmail.com>
2024-08-30 14:44:35 +0200

    (fix): require three components

--- ci/scripts/towncrier_automation.py
+++ ci/scripts/towncrier_automation.py
@@ -26,13 +26,24 @@ def parse_args(argv: Sequence[str] | None = None) -> Args:
             "The PR will be backported to main if the current branch is not main."
         ),
     )
-    parser.add_argument("version", type=str, help="What the new version is")
+    parser.add_argument(
+        "version",
+        type=str,
+        help=(
+            "The new version for the release must have at least three parts, like `major.minor.patch` and no `major.minor`."
+            "It can have a suffix like `major.minor.patch.dev0` or `major.minor.0rc1`."
+        ),
+    )
     parser.add_argument(
         "--dry-run",
         help="Whether or not to dry-run the actual creation of the pull request",
         action="store_true",
     )
     args = parser.parse_args(argv, Args())
+    if len(version.Version(args.version).release) != 3:
+        raise ValueError(
+            f"Version argument {args.version} must contain major, minor, and patch version."
+        )
     version.parse(args.version)  # validate
     return args
 
