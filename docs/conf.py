7a49007ff5eca548fde7c097fd53fa4f541d5dc5
Philipp A <flying-sheep@web.de>
2024-09-16 10:50:32 +0200

    Backport PR #1673: Upstream release-notes directive

--- docs/conf.py
+++ docs/conf.py
@@ -128,7 +128,7 @@ intersphinx_mapping = dict(
     scipy=("https://docs.scipy.org/doc/scipy/", None),
     sklearn=("https://scikit-learn.org/stable/", None),
     zarr=("https://zarr.readthedocs.io/en/stable/", None),
-    xarray=("https://xarray.pydata.org/en/stable/", None),
+    xarray=("https://docs.xarray.dev/en/stable/", None),
 )
 qualname_overrides = {
     "h5py._hl.group.Group": "h5py.Group",
