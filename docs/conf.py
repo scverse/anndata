5867770a504f106a783c17fd4ed018ed167a3b07
Phil Schaf <flying-sheep@web.de>
2024-09-16 10:17:29 +0200

    Upstream release-notes directive

--- docs/conf.py
+++ docs/conf.py
@@ -128,7 +128,7 @@ intersphinx_mapping = dict(
     scipy=("https://docs.scipy.org/doc/scipy/", None),
     sklearn=("https://scikit-learn.org/stable/", None),
     zarr=("https://zarr.readthedocs.io/en/stable/", None),
-    xarray=("https://xarray.pydata.org/en/stable/", None),
+    xarray=("https://docs.xarray.dev/en/stable/", None),
     dask=("https://docs.dask.org/en/stable/", None),
 )
 qualname_overrides = {
