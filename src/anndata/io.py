7d67b6e5714a75653aec0aa1c8d6f9a8e5d60de8
Phil Schaf <flying-sheep@web.de>
2024-09-23 16:08:40 +0200

    there we go!

--- src/anndata/io.py
+++ src/anndata/io.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-import os
+import sys
+from typing import TYPE_CHECKING
 
 from ._core.sparse_dataset import sparse_dataset
 from ._io.h5ad import read_h5ad, write_h5ad
@@ -16,22 +17,20 @@ from ._io.read import (
 from ._io.specs import read_elem, write_elem
 from ._io.write import write_csvs, write_loom
 
+if "zarr" in sys.modules or TYPE_CHECKING:
+    from ._io.zarr import read_zarr, write_zarr
+else:
+    # Importing zarr is slow, so unless it’s already imported,
+    # we wrap these functions into shims.
+    def read_zarr(*args, **kw):
+        from ._io.zarr import read_zarr
 
-def read_zarr(*args, **kw):
-    from ._io.zarr import read_zarr
+        return read_zarr(*args, **kw)
 
-    return read_zarr(*args, **kw)
+    def write_zarr(*args, **kw):
+        from ._io.zarr import write_zarr
 
-
-def write_zarr(*args, **kw):
-    from ._io.zarr import write_zarr
-
-    return write_zarr(*args, **kw)
-
-
-if os.environ.get("READTHEDOCS", "") == "True":
-    # This makes the lazy import above into a direct one. Importing zarr is slow.
-    from ._io.zarr import read_zarr, write_zarr  # noqa: TCH004
+        return write_zarr(*args, **kw)
 
 
 __all__ = [
