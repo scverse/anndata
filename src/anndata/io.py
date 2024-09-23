5e9c7a17348c42dcfde819c3970df848ef448e50
Phil Schaf <flying-sheep@web.de>
2024-09-23 13:38:01 +0200

    just in rtd

--- src/anndata/io.py
+++ src/anndata/io.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+import os
 
 from ._core.sparse_dataset import sparse_dataset
 from ._io.h5ad import read_h5ad, write_h5ad
@@ -16,19 +16,22 @@ from ._io.read import (
 from ._io.specs import read_elem, write_elem
 from ._io.write import write_csvs, write_loom
 
-if TYPE_CHECKING:
-    from ._core.anndata import AnnData
 
-try:
-    from ._io.zarr import read_zarr, write_zarr
-except ImportError as e:
-    _zarr_exc = e
+def read_zarr(*args, **kw):
+    from ._io.zarr import read_zarr
 
-    def read_zarr(*_, **__) -> AnnData:
-        raise _zarr_exc from None
+    return read_zarr(*args, **kw)
 
-    def write_zarr(*_, **__) -> None:
-        raise _zarr_exc from None
+
+def write_zarr(*args, **kw):
+    from ._io.zarr import write_zarr
+
+    return write_zarr(*args, **kw)
+
+
+if os.environ.get("READTHEDOCS", "") != "True":
+    # This makes the lazy import above into a direct one. Importing zarr is slow.
+    from ._io.zarr import read_zarr, write_zarr  # noqa: TCH004
 
 
 __all__ = [
