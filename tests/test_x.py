1af95401d7a715ae1a02f51674d88d324d073c14
ilan-gold <ilanbassgold@gmail.com>
2025-02-12 14:48:44 +0100

    (fix): add `ImplicitModificationWarning` for setting on view of `X`

--- tests/test_x.py
+++ tests/test_x.py
@@ -2,6 +2,8 @@
 
 from __future__ import annotations
 
+import re
+
 import numpy as np
 import pandas as pd
 import pytest
@@ -9,6 +11,7 @@ from scipy import sparse
 
 import anndata as ad
 from anndata import AnnData
+from anndata._warnings import ImplicitModificationWarning
 from anndata.tests.helpers import assert_equal, gen_adata
 from anndata.utils import asarray
 
@@ -174,11 +177,25 @@ def test_set_dense_x_view_from_sparse():
     x1 = np.ones((100, 30))
     orig = ad.AnnData(x)
     view = orig[:30]
-    with pytest.warns(
-        UserWarning,
-        match=r"Trying to set a dense array with a sparse array on a view",
-    ):
+    with pytest.warns(Warning) as warninfo:
         view.X = sparse.csr_matrix(x1[:30])
+    assert len(warninfo) == 2
+    for w in warninfo:
+        if w.category is UserWarning:
+            assert (
+                re.match(
+                    r"Trying to set a dense array with a sparse array on a view",
+                    w.message.args[0],
+                )
+                is not None
+            )
+        elif w.category is ImplicitModificationWarning:
+            assert (
+                re.match(r"Modifying `X` on a view results", w.message.args[0])
+                is not None
+            )
+        else:
+            pytest.fail(f"Encountered unexpected warning {w.category}")
     assert_equal(view.X, x1[:30])
     assert_equal(orig.X[:30], x1[:30])  # change propagates through
     assert_equal(orig.X[30:], x[30:])  # change propagates through
