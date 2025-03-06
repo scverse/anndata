45c15480a332fb691666bcc8e4fa35b588fcd5ad
Isaac Virshup <ivirshup@gmail.com>
2025-03-06 11:55:17 -0800

    Chunked writing of h5py.Dataset and zarr.Array (#1624)
    * Chunked writing of h5py.Dataset and zarr.Array
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Make n-dimensional
    
    * Add some tests, which fail :(
    
    * Fix up chunking algorithm + add some types
    
    * (chore): remove unneeded check?
    
    * (fix): dispatch to chunked writing for dense arrays
    
    * (chore): remove unnecessary methods
    
    * fmt
    
    * (fix): type
    
    * (fix): remove erroneous change + default dtype
    
    * (fix): just use a setting
    
    * (chore): add comment back
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: ilan-gold <ilanbassgold@gmail.com>
    Co-authored-by: Philipp A. <flying-sheep@web.de>

--- tests/test_io_dispatched.py
+++ tests/test_io_dispatched.py
@@ -204,7 +204,5 @@ def test_io_dispatched_keys(tmp_path: Path):
     write_dispatched(f, "/", adata, callback=zarr_writer)
     _ = read_dispatched(f, zarr_reader)
 
-    assert h5ad_write_keys == zarr_write_keys
     assert sorted(h5ad_read_keys) == sorted(zarr_read_keys)
-
-    assert sorted(h5ad_write_keys) == sorted(h5ad_read_keys)
+    assert sorted(h5ad_write_keys) == sorted(zarr_write_keys)
