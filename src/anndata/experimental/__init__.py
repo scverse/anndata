8855231ff71cce6fda2de01cc6fb0b3fc8bd51fc
ilan-gold <ilanbassgold@gmail.com>
2025-03-13 16:30:11 +0100

    (fix): write for docs

--- src/anndata/experimental/__init__.py
+++ src/anndata/experimental/__init__.py
@@ -4,7 +4,7 @@ from types import MappingProxyType
 from typing import TYPE_CHECKING
 
 from .._io.specs import IOSpec, read_elem_lazy
-from .._types import Read, ReadAsync, ReadCallback, StorageType, WriteCallback
+from .._types import Read, ReadAsync, ReadCallback, StorageType, Write, WriteCallback
 from ..utils import module_get_attr_redirect
 from ._dispatch_io import read_dispatched, write_dispatched
 from .backed import read_lazy
