ca2968a69690e7ff14a82c577ef4fce6ec894740
ilan-gold <ilanbassgold@gmail.com>
2025-03-20 09:43:50 +0100

    (chore): fully test paths

--- tests/lazy/test_write.py
+++ tests/lazy/test_write.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from importlib.util import find_spec
 from typing import TYPE_CHECKING
 
-import pandas as pd
+import numpy as np
 import pytest
 
 from anndata import AnnData
@@ -18,19 +18,29 @@ pytestmark = pytest.mark.skipif(not find_spec("xarray"), reason="xarray not inst
 
 
 @pytest.mark.parametrize("fmt", ["zarr", "h5ad", "loom", "csvs"])
+@pytest.mark.parametrize("key", ["obs", "var", "obsm", "varm"])
 def test_write_error(
     tmp_path: Path,
     fmt: Literal["zarr", "h5ad", "loom", "csvs"],
+    key: Literal["obs", "var", "obsm", "varm"],
 ):
     path = tmp_path / "adata.h5ad"
-    AnnData(obs=pd.DataFrame({"column": list("abcd")})).write_h5ad(path)
-    adata_remote = read_lazy(path)
+    X = np.random.random((4, 4))
+    adata = AnnData(X=X)
+    if key.endswith("m"):
+        elem = {"df": getattr(adata, key[:-1])}
+        setattr(adata, key, elem)
+    adata.write_h5ad(path)
+    adata_lazy = read_lazy(path)
+    if key.endswith("m"):
+        adata_lazy.obs = adata_lazy.obs.to_memory()
+        adata_lazy.obs = adata_lazy.var.to_memory()
     noop_path = tmp_path / f"adata_noop.{fmt}"
     with pytest.raises(
         NotImplementedError,
         match=r"Writing AnnData objects with a Dataset2D not supported. Please use `ds.to_memory`",
     ):
-        getattr(adata_remote, f"write_{fmt}")(noop_path)
+        getattr(adata_lazy, f"write_{fmt}")(noop_path)
     assert not noop_path.exists(), (
         "Found a directory at the path at which no data should have been written"
     )
