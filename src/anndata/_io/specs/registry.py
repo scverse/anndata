f852785f43d6943e906c2e7d20aa2a1b58c8b3ab
ilan-gold <ilanbassgold@gmail.com>
2025-03-27 10:09:50 +0100

    (fix): don't need to report the key/store name

--- src/anndata/_io/specs/registry.py
+++ src/anndata/_io/specs/registry.py
@@ -348,7 +348,7 @@ class Writer:
 
         # we allow stores to have a prefix like /uns which are then written to with keys like /uns/foo
         if "/" in k.split(store.name)[-1][1:]:
-            msg = f"Forward slashes are not allowed in keys when writing - please remove the forward slash found at {k} on {store}"
+            msg = "Forward slashes are not allowed in keys when writing."
             raise ValueError(msg)
 
         if isinstance(store, h5py.File):
