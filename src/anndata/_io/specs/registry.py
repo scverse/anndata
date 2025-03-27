52d3cf4cd1403b5edd59ff611da486ee13ce911b
Ilan Gold <ilanbassgold@gmail.com>
2025-03-27 13:25:47 +0100

    (fix): disallow forward slash writes (#1940)
    * (fix): disallow forward slash writes
    
    * (chore): relnote
    
    * (fix): allow `/` leading-key
    
    * (fix): allow correct keys
    
    * (fix): don't need to report the key/store name
    
    * (fix): no need to say we are writing twice

--- src/anndata/_io/specs/registry.py
+++ src/anndata/_io/specs/registry.py
@@ -346,6 +346,11 @@ class Writer:
 
         import h5py
 
+        # we allow stores to have a prefix like /uns which are then written to with keys like /uns/foo
+        if "/" in k.split(store.name)[-1][1:]:
+            msg = "Forward slashes are not allowed in keys."
+            raise ValueError(msg)
+
         if isinstance(store, h5py.File):
             store = store["/"]
 
