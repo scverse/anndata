341850d97b3a5899fa7b6a87ec3e52bb6cdcc38a
ilan-gold <ilanbassgold@gmail.com>
2025-05-16 15:31:27 +0200

    (fix): check xarray installation when reading in a dataframe

--- src/anndata/_io/specs/lazy_methods.py
+++ src/anndata/_io/specs/lazy_methods.py
@@ -12,7 +12,7 @@ from scipy import sparse
 
 import anndata as ad
 from anndata._core.file_backing import filename, get_elem_name
-from anndata._core.xarray import Dataset2D
+from anndata._core.xarray import Dataset2D, requires_xarray
 from anndata.abc import CSCDataset, CSRDataset
 from anndata.compat import DaskArray, H5Array, H5Group, XDataArray, ZarrArray, ZarrGroup
 
@@ -258,6 +258,7 @@ DUMMY_RANGE_INDEX_KEY = "_anndata_dummy_range_index"
 
 @_LAZY_REGISTRY.register_read(ZarrGroup, IOSpec("dataframe", "0.2.0"))
 @_LAZY_REGISTRY.register_read(H5Group, IOSpec("dataframe", "0.2.0"))
+@requires_xarray
 def read_dataframe(
     elem: H5Group | ZarrGroup,
     *,
@@ -297,6 +298,7 @@ def read_dataframe(
 
 @_LAZY_REGISTRY.register_read(ZarrGroup, IOSpec("categorical", "0.2.0"))
 @_LAZY_REGISTRY.register_read(H5Group, IOSpec("categorical", "0.2.0"))
+@requires_xarray
 def read_categorical(
     elem: H5Group | ZarrGroup,
     *,
@@ -317,6 +319,7 @@ def read_categorical(
     )
 
 
+@requires_xarray
 def read_nullable(
     elem: H5Group | ZarrGroup,
     *,
