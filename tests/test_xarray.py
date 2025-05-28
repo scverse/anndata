d5bf6d5d7bb1389459d9c09281de943e0a6e1b06
ilan-gold <ilanbassgold@gmail.com>
2025-05-28 14:48:42 +0200

    (fIx): move tests

--- tests/test_xarray.py
+++ tests/test_xarray.py
@@ -7,6 +7,7 @@ import pandas as pd
 import pytest
 
 from anndata._core.xarray import Dataset2D
+from anndata.compat import XDataArray
 from anndata.tests.helpers import gen_typed_df
 
 
@@ -89,3 +90,112 @@ def test_index(dataset2d):
     assert np.all(dataset2d.index == new_idx)
     assert dataset2d.true_index_dim == dataset2d.index_dim == new_idx.name
     assert list(dataset2d.coords.keys()) == [new_idx.name]
+
+
+@pytest.fixture
+def dataset_2d_one_column():
+    return Dataset2D(
+        {"foo": ("obs_names", pd.array(["a", "b", "c"], dtype="category"))},
+        coords={"obs_names": [1, 2, 3]},
+    )
+
+
+def test_dataset_2d_set_dataarray(dataset_2d_one_column):
+    da = XDataArray(
+        np.arange(3), coords={"obs_names": [1, 2, 3]}, dims=("obs_names"), name="bar"
+    )
+    dataset_2d_one_column["bar"] = da
+    assert dataset_2d_one_column["bar"].dims == ("obs_names",)
+    assert dataset_2d_one_column["bar"].equals(da)
+
+
+@pytest.mark.parametrize(
+    "setter",
+    [
+        pd.array(["e", "f", "g"], dtype="category"),
+        ("obs_names", pd.array(["e", "f", "g"], dtype="category")),
+    ],
+    ids=["array", "tuple_with_array"],
+)
+def test_dataset_2d_set_extension_array(dataset_2d_one_column, setter):
+    dataset_2d_one_column["bar"] = setter
+    assert dataset_2d_one_column["bar"].dims == ("obs_names",)
+    assert (
+        dataset_2d_one_column["bar"].data is setter[1]
+        if isinstance(setter, tuple)
+        else setter
+    )
+
+
+@pytest.mark.parametrize(
+    ("da", "msg"),
+    [
+        pytest.param(
+            XDataArray(
+                np.arange(3),
+                coords={"foo": ("obs_names", np.arange(3))},
+                dims="obs_names",
+                name="bar",
+            ),
+            "DataArray should have coordinate obs_names",
+            id="coord_name",
+        ),
+        pytest.param(
+            XDataArray(
+                np.arange(3),
+                coords={"obs_names": np.arange(3)},
+                dims=("obs_names",),
+                name="not_bar",
+            ),
+            "DataArray should have name bar, found not_bar",
+            id="dataarray_name",
+        ),
+        pytest.param(
+            XDataArray(
+                np.arange(9).reshape(3, 3),
+                coords={"obs_names": np.arange(3), "not_obs_names": np.arange(3)},
+                dims=("obs_names", "not_obs_names"),
+            ),
+            "should have only one dimension",
+            id="multiple_dims",
+        ),
+        pytest.param(
+            XDataArray(
+                np.arange(3),
+                coords=[np.arange(3)],
+                dims="not_obs_names",
+            ),
+            "dimension obs_names, found not_obs_names",
+            id="name_conflict",
+        ),
+        pytest.param(
+            ("not_obs_names", [1, 2, 3]),
+            "Setting value tuple should have first entry",
+            id="tuple_bad_dim",
+        ),
+        pytest.param(
+            (("not_obs_names",), [1, 2, 3]),
+            "Dimension tuple should have only",
+            id="nested_tuple_bad_dim",
+        ),
+        pytest.param(
+            (("obs_names", "bar"), [1, 2, 3]),
+            "Dimension tuple is too long",
+            id="nested_tuple_too_long",
+        ),
+    ],
+)
+def test_dataset_2d_set_with_bad_dataarray(da, msg, dataset_2d_one_column):
+    with pytest.raises(ValueError, match=msg):
+        dataset_2d_one_column["bar"] = da
+
+
+@pytest.mark.parametrize(
+    "data", [np.arange(3), XDataArray(np.arange(3), dims="obs_names", name="obs_names")]
+)
+def test_dataset_2d_set_index(data, dataset_2d_one_column):
+    with pytest.raises(
+        KeyError,
+        match="Cannot set obs_names as a variable",
+    ):
+        dataset_2d_one_column["obs_names"] = data
