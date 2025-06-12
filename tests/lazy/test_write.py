60067f8111e86e1d0decf1596af7dffa00a1181e
ilan-gold <ilanbassgold@gmail.com>
2025-06-12 17:17:30 +0200

    (fix): go back to `pytestmark` where fixtures not created

--- tests/lazy/test_write.py
+++ tests/lazy/test_write.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from importlib.util import find_spec
 from typing import TYPE_CHECKING
 
 import numpy as np
@@ -13,7 +14,7 @@ if TYPE_CHECKING:
     from typing import Literal
 
 
-pytest.importorskip("xarray")
+pytestmark = pytest.mark.skipif(not find_spec("xarray"), reason="xarray not installed")
 
 
 @pytest.mark.parametrize("fmt", ["zarr", "h5ad", "loom", "csvs"])
