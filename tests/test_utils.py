e38e79239711bb6806b63fd9cc038ebe43a1309a
ilan-gold <ilanbassgold@gmail.com>
2025-06-20 16:10:13 +0200

    (fix): lint

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -13,7 +13,7 @@ from anndata.utils import make_index_unique
 
 def test_make_index_unique():
     index = pd.Index(["val", "val", "val-1", "val-1"])
-    with pytest.warns(UserWarning):
+    with pytest.warns(UserWarning, match="Suffix used"):
         result = make_index_unique(index)
     expected = pd.Index(["val", "val-2", "val-1", "val-1-1"])
     assert list(expected) == list(result)
