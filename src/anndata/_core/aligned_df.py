cddcf0b5709049ac7f27f9746ba608fd3a75f6aa
ilan-gold <ilanbassgold@gmail.com>
2025-08-27 18:22:11 +0200

    fix: explicit error for `pandas.MultiIndex`

--- src/anndata/_core/aligned_df.py
+++ src/anndata/_core/aligned_df.py
@@ -78,6 +78,9 @@ def _gen_dataframe_df(
     attr: Literal["obs", "var"],
     length: int | None = None,
 ):
+    if isinstance(anno.index, pd.MultiIndex):
+        msg = "pandas.MultiIndex not supported as index for obs or var"
+        raise ValueError(msg)
     if length is not None and length != len(anno):
         raise _mk_df_error(source, attr, length, len(anno))
     anno = anno.copy(deep=False)
