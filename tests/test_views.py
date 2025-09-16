1920dd6392684d5a53fdbfb8885442c69286696e
Lumberbot (aka Jack) <39504233+meeseeksmachine@users.noreply.github.com>
2025-09-16 07:33:09 -0700

    Backport PR #2053 on branch 0.12.x (fix: unbound dask) (#2120)
    Co-authored-by: Ilan Gold <ilanbassgold@gmail.com>

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from contextlib import ExitStack
+from contextlib import ExitStack, nullcontext
 from copy import deepcopy
 from operator import mul
 from typing import TYPE_CHECKING
@@ -22,7 +22,7 @@ from anndata._core.views import (
     SparseCSRArrayView,
     SparseCSRMatrixView,
 )
-from anndata.compat import CupyCSCMatrix, DaskArray
+from anndata.compat import CSArray, CupyCSCMatrix, DaskArray
 from anndata.tests.helpers import (
     BASE_MATRIX_PARAMS,
     CUPY_MATRIX_PARAMS,
@@ -189,13 +189,26 @@ def test_modify_view_component(matrix_type, mapping_name, request):
     with pytest.warns(ad.ImplicitModificationWarning, match=rf".*\.{mapping_name}.*"):
         m[0, 0] = 100
     assert not subset.is_view
-    assert getattr(subset, mapping_name)["m"][0, 0] == 100
+    # TODO: Remove `raises` after https://github.com/scipy/scipy/pull/23626.
+    import dask
+
+    is_dask_with_broken_view_setting = (
+        "sparse_dask" in request.node.callspec.id
+        and Version(dask.__version__) >= Version("2025.02.0")
+    )
+    is_sparse_array_in_lower_dask_version = (
+        not is_dask_with_broken_view_setting
+        and isinstance(m, DaskArray)
+        and isinstance(m._meta, CSArray)
+    )
+    with (
+        pytest.raises(ValueError, match=r"shape mismatch")
+        if is_sparse_array_in_lower_dask_version or is_dask_with_broken_view_setting
+        else nullcontext()
+    ):
+        assert getattr(subset, mapping_name)["m"][0, 0] == 100
 
     assert init_hash == hash_func(adata)
-
-    if "sparse_array_dask_array" in request.node.callspec.id:
-        msg = "sparse arrays in dask are generally expected to fail but in this case they do not"
-        pytest.fail(msg)
 
 
 @pytest.mark.parametrize("attr", ["obsm", "varm"])
