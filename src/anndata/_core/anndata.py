0fae2dcd7e2247b2e35c0072236d43c76e033c95
Philipp A. <flying-sheep@web.de>
2025-09-26 12:05:08 +0200

    no override

--- src/anndata/_core/anndata.py
+++ src/anndata/_core/anndata.py
@@ -1054,8 +1054,7 @@ class AnnData(metaclass=utils.DeprecationMixinMeta):  # noqa: PLW1641
             if not isinstance(df_full[k].dtype, pd.CategoricalDtype):
                 continue
             all_categories = df_full[k].cat.categories
-            with pd.option_context("mode.chained_assignment", None):
-                df_sub[k] = df_sub[k].cat.remove_unused_categories()
+            df_sub[k] = df_sub[k].cat.remove_unused_categories()
             # also correct the colors...
             color_key = f"{k}_colors"
             if color_key not in uns:
