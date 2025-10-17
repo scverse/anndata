150b59607bade2db0fb8910a4a3d8e519a6371a4
ilan-gold <ilanbassgold@gmail.com>
2025-10-17 13:26:40 +0200

    fix: add more benchmarks + fix for string arrays

--- benchmarks/benchmarks/dataset2d.py
+++ benchmarks/benchmarks/dataset2d.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import tempfile
 from pathlib import Path
+from types import MappingProxyType
 from typing import TYPE_CHECKING
 
 import h5py
@@ -13,10 +14,19 @@ import anndata as ad
 
 if TYPE_CHECKING:
     from collections.abc import Callable
+    from typing import Literal
 
 
 class Dataset2D:
-    param_names = ("gen_store", "chunks")
+    param_names = ("gen_store", "chunks", "gen_array_key")
+    arr_generator = MappingProxyType({
+        "cat": lambda n_obs: pd.Categorical(np.array(["a"] * n_obs)),
+        "numeric": lambda n_obs: np.arange(n_obs),
+        "string-array": lambda n_obs: np.array(["a"] * n_obs),
+        "nullable-string-array": lambda n_obs: pd.array(
+            ["a", pd.NA] * (n_obs // 2), dtype="string"
+        ),
+    })
     params = (
         (
             lambda: h5py.File(Path(tempfile.mkdtemp()) / "data.h5ad", mode="w"),
@@ -25,22 +35,38 @@ class Dataset2D:
             ),
         ),
         ((-1,), None),
+        ("cat", "numeric", "string-array", "nullable-string-array"),
     )
 
     def setup(
-        self, gen_store: Callable[[], zarr.Group | h5py.File], chunks: None | tuple[int]
+        self,
+        gen_store: Callable[[], zarr.Group | h5py.File],
+        chunks: None | tuple[int],
+        gen_array_key: Literal[
+            "cat", "numeric", "string-array", "nullable-string-array"
+        ],
     ):
         self.n_obs = 100000
         df = pd.DataFrame(
-            {
-                "a": pd.Categorical(np.array(["a"] * self.n_obs)),
-                "b": np.arange(self.n_obs),
-            },
+            {"a": self.arr_generator[gen_array_key](self.n_obs)},
             index=[f"cell{i}" for i in range(self.n_obs)],
         )
-        store = gen_store()
-        ad.io.write_elem(store, "obs", df)
-        self.ds = ad.experimental.read_elem_lazy(store["obs"], chunks=chunks)
+        # Force writing string-array format to check read performance
+        if (
+            isinstance(self.arr_generator[gen_array_key](1), np.ndarray)
+            and df["a"].dtype == "string"
+        ):
+            df["a"] = df["a"].to_numpy()
+        self.store = gen_store()
+        with ad.settings.override(allow_write_nullable_strings=True):
+            ad.io.write_elem(self.store, "obs", df)
+        self.ds = ad.experimental.read_elem_lazy(self.store["obs"], chunks=chunks)
+
+    def time_read_lazy_default(self, *_):
+        ad.experimental.read_elem_lazy(self.store["obs"])
+
+    def peakmem_read_lazy_default(self, *_):
+        ad.experimental.read_elem_lazy(self.store["obs"])
 
     def time_getitem_slice(self, *_):
         self.ds.iloc[0 : (self.n_obs // 2)].to_memory()
