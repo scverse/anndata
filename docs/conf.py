2c275bd903c2604251e5e10fb54d8fd13432a2a1
Philipp A. <flying-sheep@web.de>
2025-11-11 12:40:54 +0100

    docs: fix rendering of `anndata.typing.*` types (#2211)

--- docs/conf.py
+++ docs/conf.py
@@ -92,10 +92,6 @@ nitpick_ignore = [  # APIs without an intersphinx entry
     ("py:class", "anndata._core.raw.Raw"),
     # TODO: remove zappy support; the zappy repo is archived
     ("py:class", "anndata.compat.ZappyArray"),
-    # TODO: remove once https://github.com/sphinx-doc/sphinx/pull/13508 is released
-    ("py:class", "anndata.typing.TypeAliasType"),
-    ("py:class", "anndata._types.TypeAliasType"),
-    ("py:class", "anndata._io.specs.registry.TypeAliasType"),
 ]
 
 
