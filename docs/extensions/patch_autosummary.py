eeb3fd200b4fd64ca5b09497464433a11fb3011f
Philipp A. <flying-sheep@web.de>
2026-01-15 14:44:03 +0100

    use public APIs

--- docs/extensions/patch_autosummary.py
+++ docs/extensions/patch_autosummary.py
@@ -5,6 +5,7 @@
 
 from __future__ import annotations
 
+import sys
 from traceback import walk_stack
 from typing import TYPE_CHECKING
 
@@ -13,6 +14,11 @@ if TYPE_CHECKING:
 
     from sphinx.application import Sphinx
     from sphinx.ext.autodoc import Options
+
+if sys.version_info >= (3, 13):
+    from typing import get_protocol_members, is_protocol
+else:
+    from typing_extensions import get_protocol_members, is_protocol
 
 
 def skip_member(  # noqa: PLR0917
@@ -43,7 +49,7 @@ def skip_member(  # noqa: PLR0917
         return None
 
     # If we’re documenting a protocol attribute, include it
-    if name in getattr(parent, "__protocol_attrs__", ()):
+    if is_protocol(parent) and name in get_protocol_members(parent):
         return False
 
     # Skip if it’s not a member of the parent class
