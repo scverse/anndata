d76a96e91a030cf4a6b3653d15e5fc8fc18c9a9e
ilan-gold <ilanbassgold@gmail.com>
2026-02-04 15:55:08 +0100

    chore: more description

--- src/anndata/_core/anndata.py
+++ src/anndata/_core/anndata.py
@@ -625,7 +625,9 @@ class AnnData(metaclass=utils.DeprecationMixinMeta):  # noqa: PLW1641
                 self._init_as_actual(new)
                 return None
             else:
-                msg = "Setting element `.X` of view will obey copy-on-write semantics in the next minor release. Set `anndata.settings.copy_on_write_X = True` to begin opting in to this behavior."
+                msg = "Setting element `.X` of view of `AnnData` object will obey copy-on-write semantics in the next minor release."
+                "In other words, this subset of your original `AnnData` will be copied-in-place and initialized with the value passed into this setter. "
+                "Set `anndata.settings.copy_on_write_X = True` to begin opting in to this behavior."
                 warn(msg, FutureWarning)
         if value is None:
             if self.isbacked:
