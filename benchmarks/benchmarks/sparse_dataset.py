5de57ef94ad6bd97d4fd041d1a532bf11b840a7e
ilan-gold <ilanbassgold@gmail.com>
2026-04-20 10:39:17 +0200

    fix: bigger benchmark

--- benchmarks/benchmarks/sparse_dataset.py
+++ benchmarks/benchmarks/sparse_dataset.py
@@ -113,7 +113,7 @@ class SparseCSRDaskConcat:
                 ),
                 X=read_elem_lazy(self.group["X"]),
             )
-            for i in range(10)
+            for i in range(20)
         ]
 
     def time_concat(self, join: Literal["inner", "outer"]):
