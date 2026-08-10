88ac35b4dfd468971b9295be7bb3fbd12c809902
Phil Schaf <flying-sheep@web.de>
2026-08-10 11:25:24 +0200

    less bespoke

--- src/anndata/_core/access.py
+++ src/anndata/_core/access.py
@@ -16,32 +16,18 @@ class ElementRef[K: (str, str | None)](NamedTuple):
         return f".{self.attrname}" + "".join(f"[{x!r}]" for x in self.keys)
 
     @property
-    def _container(self):
-        """The collection holding `keys[-1]`, i.e. one level up from the value."""
+    def _parent_el(self):
         return reduce(
             lambda d, k: d[k], self.keys[:-1], getattr(self.parent, self.attrname)
         )
 
-    @property
-    def unsubset_element(self):
-        """Get referenced value as it exists before `self.parent`’s subsetting.
-
-        `None` if `self.parent` is not a view, and so has no unsubset value.
-        """
-        if (adata_ref := self.parent._adata_ref) is None:
-            return None
-        return self._get_in(adata_ref)
-
-    def _get_in(self, adata: AnnData):
-        return reduce(lambda d, k: d[k], self.keys, getattr(adata, self.attrname))
-
     def get(self):
         """Get referenced value in self.parent."""
-        return self._get_in(self.parent)
+        return reduce(lambda d, k: d[k], self.keys, getattr(self.parent, self.attrname))
 
     def set(self, val):
         """Set referenced value in self.parent."""
-        self._container[self.keys[-1]] = val
+        self._parent_el[self.keys[-1]] = val
 
     def delete(self):
-        del self._container[self.keys[-1]]
+        del self._parent_el[self.keys[-1]]
